86fb27d8abd38c67ee2f42460ccc2a5c94cf61dc
lianxmfor <1034552569@qq.com>
2021-09-24 15:40:55 +0800

    option -> opt

--- featctl/pkg/query/query.go
+++ featctl/pkg/query/query.go
@@ -24,51 +24,51 @@ type Option struct {
 
 var firstPrint = true
 
-func Run(ctx context.Context, option *Option) {
-	db, err := database.Open(&option.DBOption)
+func Run(ctx context.Context, opt *Option) {
+	db, err := database.Open(&opt.DBOption)
 	if err != nil {
 		log.Fatalf("failed connecting feature store: %v", err)
 	}
 	defer db.Close()
 
-	if err := queryFeatureAndPrintToStdout(ctx, db, option); err != nil {
+	if err := queryFeatureAndPrintToStdout(ctx, db, opt); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func queryFeatureAndPrintToStdout(ctx context.Context, db *database.DB, option *Option) error {
-	entityTableMapFeatures, err := getEntityTableMapFeatures(ctx, db, option)
+func queryFeatureAndPrintToStdout(ctx context.Context, db *database.DB, opt *Option) error {
+	entityTableMapFeatures, err := getEntityTableMapFeatures(ctx, db, opt)
 	if err != nil {
 		return err
 	}
 
 	w := csv.NewWriter(os.Stdout)
 	for entityTable, featureNames := range entityTableMapFeatures {
-		if err := readOneTableToCsv(ctx, db, entityTable, option.Entitykeys, featureNames, w); err != nil {
+		if err := readOneTableToCsv(ctx, db, entityTable, opt.Entitykeys, featureNames, w); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func getEntityTableMapFeatures(ctx context.Context, db *database.DB, op *Option) (map[string][]string, error) {
+func getEntityTableMapFeatures(ctx context.Context, db *database.DB, opt *Option) (map[string][]string, error) {
 	mp := make(map[string][]string)
 
-	if op.Revision != "" {
-		entityTable := op.Group + "_" + op.Revision
-		mp[entityTable] = op.FeatureNames
+	if opt.Revision != "" {
+		entityTable := opt.Group + "_" + opt.Revision
+		mp[entityTable] = opt.FeatureNames
 		return mp, nil
 	}
 
-	for _, featureName := range op.FeatureNames {
-		if entityTable, err := getEntityTable(ctx, db, op.Group, featureName); err == nil && entityTable != "" {
+	for _, featureName := range opt.FeatureNames {
+		if entityTable, err := getEntityTable(ctx, db, opt.Group, featureName); err == nil && entityTable != "" {
 			if v, ok := mp[entityTable]; ok {
 				mp[entityTable] = append(v, featureName)
 			} else {
 				mp[entityTable] = []string{featureName}
 			}
 		} else {
-			return nil, fmt.Errorf("cannot find entity table for group=%s, featurename=%s, err: %v", op.Group, featureName, err)
+			return nil, fmt.Errorf("cannot find entity table for group=%s, featurename=%s, err: %v", opt.Group, featureName, err)
 		}
 	}
 	return mp, nil
