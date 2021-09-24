a10252a07edf4d0c9e9e87e3d1f8b11a05848a2c
yingjinghan14421 <yingjinghan14421@ipalfish.com>
2021-09-24 14:56:51 +0800

    feat(featctl/pkg): add method List, list all existing features

---
+++ featctl/pkg/list_feature/list_feature.go
@@ -0,0 +1,51 @@
+package list_feature
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+
+	database2 "github.com/onestore-ai/onestore/featctl/pkg/database"
+	"github.com/onestore-ai/onestore/pkg/database"
+)
+
+type Option struct {
+	Group    string
+	DBOption database2.Option
+}
+
+func ListFeature(ctx context.Context, option *Option) {
+	sqlxDBOption := buildSqlxDBOption(option.DBOption)
+	db, err := database.Open(sqlxDBOption)
+	if err != nil {
+		log.Fatalf("failed connecting feature store: %v", err)
+	}
+	defer db.Close()
+
+	features, err := database.ListFeatureConfigByGroup(db, option.Group)
+	if err != nil {
+		log.Fatalf("failed listing feature configs in group %s: %v", option.Group, err)
+	}
+
+	fmt.Println(featureConfigTitle())
+	for _, feature := range features {
+		fmt.Println(feature.OneLineString())
+	}
+}
+
+func featureConfigTitle() string {
+	return strings.Join([]string{
+		"Name", "Group", "Revision", "Status", "Category", "ValueType", "Description", "RevisionsLimit", "CreateTime", "ModifyTime"},
+		",")
+}
+
+func buildSqlxDBOption(option database2.Option) *database.Option{
+	return &database.Option{
+		Host: option.Host,
+		Port: option.Port,
+		User: option.User,
+		Pass: option.Pass,
+		DbName: option.DbName,
+	}
+}
