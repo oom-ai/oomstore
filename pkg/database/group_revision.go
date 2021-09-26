eef183f0d15c445c893104d1c8952971db55040f
Jinghan Ying <jinghancc@gmail.com>
2021-09-26 16:16:00 +0800

    feat(pkg/database): add database layer for group_revision

---
+++ pkg/database/group_revision.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type GroupRevision struct {
+	Group       string    `db:"group"`
+	Revision    string    `db:"revision"`
+	Source      string    `db:"source"`
+	Description string    `db:"description"`
+	CreateTime  time.Time `db:"create_time"`
+	ModifyTime  time.Time `db:"modify_time"`
+}
+
+func ListGroupRevisionByGroup(db *sqlx.DB, group string) ([]GroupRevision, error) {
+	query := fmt.Sprintf(`SELECT * FROM feature_revision AS fr WHERE fr.group = '%s'`, group)
+	revisions := make([]GroupRevision, 0)
+	if err := db.Select(&revisions, query); err != nil {
+		return nil, err
+	}
+	return revisions, nil
+}
+
+func (r *GroupRevision) OneLineString() string {
+	return strings.Join([]string{
+		r.Group, r.Revision, r.Source, r.Description, r.CreateTime.Format(time.RFC3339), r.ModifyTime.Format(time.RFC3339)},
+		",")
+}
