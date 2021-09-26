fc0fbf2555b1b44ad02f92c6d2275ec900104b7d
Jinghan Ying <jinghancc@gmail.com>
2021-09-26 16:16:51 +0800

    feat(featctl): implement cmd list revision

---
+++ featctl/cmd/list_revision.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"context"
+
+	"github.com/onestore-ai/onestore/featctl/pkg/list_revision"
+	"github.com/spf13/cobra"
+)
+
+var listRevisionOpt list_revision.Option
+
+// listRevisionCmd represents the list revision command
+var listRevisionCmd = &cobra.Command{
+	Use:   "revision",
+	Short: "list historical revisions given a specific group",
+	Run: func(cmd *cobra.Command, args []string) {
+		listRevisionOpt.DBOption = dbOption
+		list_revision.ListRevision(context.Background(), &listRevisionOpt)
+	},
+}
+
+func init() {
+	listCmd.AddCommand(listRevisionCmd)
+
+	flags := listRevisionCmd.Flags()
+
+	flags.StringVarP(&listRevisionOpt.Group, "group", "g", "", "feature group")
+	_ = listRevisionCmd.MarkFlagRequired("group")
+}
