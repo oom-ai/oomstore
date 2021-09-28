7350f74e7e3137817b8355c8a379ba7047c01042
Wenxuan Zhang <wenxuangm@gmail.com>
2021-09-28 16:59:29 +0800

    test: update `featctl export` test

--- featctl/test/test_export.sh
+++ featctl/test/test_export.sh
@@ -7,16 +7,16 @@ import_sample v1
 register_features v1
 
 case='featctl export works'
-expected='"entity_key","price"
-"1",3999
-"2",5299
-"3",3999
-"4",1999
-"5",999
-"6",4999
-"7",5999
-"8",6500
-"9",4500
+expected='entity_key,price
+1,3999
+2,5299
+3,3999
+4,1999
+5,999
+6,4999
+7,5999
+8,6500
+9,4500
 '
 trap 'command rm -rf price.csv' EXIT INT TERM HUP
 featctl export -g device -n price --output-file price.csv &>/dev/null
