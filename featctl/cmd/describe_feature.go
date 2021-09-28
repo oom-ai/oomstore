f67d2867b5752679807c7398ccc815fe2857f09a
Wenxuan Zhang <wenxuangm@gmail.com>
2021-09-28 11:44:41 +0800

    refactor(featctl): rename `describe` command

---
+++ featctl/cmd/describe_feature.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"context"
+
+	"github.com/onestore-ai/onestore/featctl/pkg/describe_feature"
+	"github.com/spf13/cobra"
+)
+
+var describeFeatureOpt describe_feature.Option
+
+// describeCmd represents the describe command
+var describeFeatureCmd = &cobra.Command{
+	Use:   "feature",
+	Short: "show details of a specific feature",
+	Run: func(cmd *cobra.Command, args []string) {
+		describeFeatureOpt.DBOption = dbOption
+		describe_feature.Run(context.Background(), &describeFeatureOpt)
+	},
+}
+
+func init() {
+	describeCmd.AddCommand(describeFeatureCmd)
+
+	flags := describeFeatureCmd.Flags()
+
+	flags.StringVarP(&describeFeatureOpt.Group, "group", "g", "", "feature group")
+	_ = describeFeatureCmd.MarkFlagRequired("group")
+
+	flags.StringVarP(&describeFeatureOpt.Name, "name", "n", "", "feature name")
+	_ = describeFeatureCmd.MarkFlagRequired("name")
+}
