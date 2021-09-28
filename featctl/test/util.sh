5b24c3db03736b83bbe546d400bf5f7fa9b12c70
Wenxuan Zhang <wenxuangm@gmail.com>
2021-09-28 14:55:16 +0800

    test: add `import` test

--- featctl/test/util.sh
+++ featctl/test/util.sh
@@ -49,6 +49,8 @@ assert_eq() {
 import_sample() {
     local revision=$1
     info "import sample data $revision"
+    trap 'command rm -rf -- lightning.* *.tmp' EXIT INT TERM HUP
+
     featctl import \
     --group device \
     --revision "$revision" \
@@ -56,8 +58,6 @@ import_sample() {
     --input-file device.csv \
     --has-header \
     --description 'test data' &>/dev/null
-
-    rm -rf -- lightning.* *.tmp
 }
 
 # register features in sample data with specified revision
