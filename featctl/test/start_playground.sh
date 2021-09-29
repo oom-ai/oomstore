2de39182ddc8647bccdcb86130305e5afd39a4aa
Wenxuan Zhang <wenxuangm@gmail.com>
2021-09-29 20:49:35 +0800

    test: using mysql as playground

--- featctl/test/start_playground.sh
+++ featctl/test/start_playground.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
-info() { printf "$(date -Is) %b[info]%b %s\n" '\e[0;32m\033[1m' '\e[0m' "$*" >&2; }
-
 docker run --rm -d --name onestore \
-    -p 2379:2379 \
-    -p 9090:9090 \
-    -p 3000:3000 \
-    -p 4000:4000 \
-    -p 3930:3930 \
-    aiinfra/tidb-playground >/dev/null
+    -e MYSQL_ALLOW_EMPTY_PASSWORD=1 \
+    -p 4000:3306 \
+    mysql:5.7 >/dev/null
 
-docker logs -f onestore | sed -E 's/[0-9]+.[0-9]+.[0-9]+.[0-9]+/127.0.0.1/g'
+docker logs -f onestore
