45b613bf96d496c50bdcfd5e145db940d58ad655
lianxmfor <1034552569@qq.com>
2021-10-13 18:55:37 +0800

    feat: implement api listRevision

---
+++ pkg/onestore/revision.go
@@ -0,0 +1,11 @@
+package onestore
+
+import (
+	"context"
+
+	"github.com/onestore-ai/onestore/pkg/onestore/types"
+)
+
+func (s *OneStore) ListRevision(ctx context.Context, groupName string) ([]*types.Revision, error) {
+	return s.db.ListRevision(ctx, groupName)
+}
