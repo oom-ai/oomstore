c18a2e75f5a4e89f4b8c029e0d5cecbe61aec5b4
Wenxuan <wenxuangm@gmail.com>
2021-10-13 17:37:39 +0800

    feat: implement api `Create` (#91)

--- pkg/onestore/onestore.go
+++ pkg/onestore/onestore.go
@@ -1,9 +1,39 @@
 package onestore
 
 import (
+	"context"
+
 	"github.com/onestore-ai/onestore/internal/database"
+	"github.com/onestore-ai/onestore/pkg/onestore/types"
 )
 
 type OneStore struct {
 	db *database.DB
+}
+
+func Open(ctx context.Context, opt types.OneStoreOpt) (*OneStore, error) {
+	db, err := database.Open(toDatabaseOption(&opt))
+	if err != nil {
+		return nil, err
+	}
+
+	return &OneStore{db}, nil
+}
+
+func Create(ctx context.Context, opt types.OneStoreOpt) (*OneStore, error) {
+	if err := database.CreateDatabase(ctx, toDatabaseOption(&opt)); err != nil {
+		return nil, err
+	}
+
+	return Open(ctx, opt)
+}
+
+func toDatabaseOption(opt *types.OneStoreOpt) database.Option {
+	return database.Option{
+		Host:   opt.Host,
+		Port:   opt.Port,
+		User:   opt.User,
+		Pass:   opt.Pass,
+		DbName: opt.Workspace,
+	}
 }
