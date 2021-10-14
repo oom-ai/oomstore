a40049126eec14c8246e2a8438c89ba8a23298a2
lianxmfor <1034552569@qq.com>
2021-10-14 10:22:43 +0800

    refactor(group): rename Group to featureGroup

--- internal/database/feature_group.go
+++ internal/database/feature_group.go
@@ -6,14 +6,14 @@ import (
 	"github.com/onestore-ai/onestore/pkg/onestore/types"
 )
 
-func (db *DB) CreateGroup(ctx context.Context, opt types.CreateGroupOpt) error {
+func (db *DB) CreateFeatureGroup(ctx context.Context, opt types.CreateFeatureGroupOpt) error {
 	_, err := db.ExecContext(ctx,
 		"insert into feature_group(name, entity_name, category, description) values(?, ?, ?, ?)",
 		opt.Name, opt.EntityName, opt.Category, opt.Description)
 	return err
 }
 
-func (db *DB) GetGroup(ctx context.Context, groupName string) (*types.FeatureGroup, error) {
+func (db *DB) GetFeatureGroup(ctx context.Context, groupName string) (*types.FeatureGroup, error) {
 	var group types.FeatureGroup
 	query := `SELECT * FROM feature_group WHERE name = ?`
 	if err := db.GetContext(ctx, &group, query, groupName); err != nil {
