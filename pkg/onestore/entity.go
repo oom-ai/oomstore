5661d381bf3883eaf697a461216f79fa9ac45453
Wenxuan Zhang <wenxuangm@gmail.com>
2021-10-15 16:19:13 +0800

    feat: add `GetEntity` api

--- pkg/onestore/entity.go
+++ pkg/onestore/entity.go
@@ -11,19 +11,14 @@ func (s *OneStore) CreateEntity(ctx context.Context, opt types.CreateEntityOpt) 
 	if err := s.db.CreateEntity(ctx, opt); err != nil {
 		return nil, err
 	}
+	return s.GetEntity(ctx, opt.Name)
+}
 
-	return &types.Entity{
-		Name:        opt.Name,
-		Length:      opt.Length,
-		Description: opt.Description,
-	}, nil
+func (s *OneStore) GetEntity(ctx context.Context, name string) (*types.Entity, error) {
+	return s.db.GetEntity(ctx, name)
 }
 
 // ListEntity: get all entity
 func (s *OneStore) ListEntity(ctx context.Context) ([]*types.Entity, error) {
-	entities, err := s.db.ListEntity(ctx)
-	if err != nil {
-		return nil, err
-	}
-	return entities, nil
+	return s.db.ListEntity(ctx)
 }
