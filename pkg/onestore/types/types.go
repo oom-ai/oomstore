74cb2b19846b3ac75a0dc6ab88e456aa4dbd8fe6
Jinghan Ying <jinghancc@gmail.com>
2021-10-18 15:40:24 +0800

    feat(pkg/onestore): add String() method for entity

--- pkg/onestore/types/types.go
+++ pkg/onestore/types/types.go
@@ -104,3 +104,13 @@ func (rf *RichFeature) String() string {
 		fmt.Sprintf("ModifyTime:    %s", rf.ModifyTime.Format(time.RFC3339)),
 	}, "\n")
 }
+
+func (e *Entity) String() string {
+	return strings.Join([]string{
+		fmt.Sprintf("Name:          %s", e.Name),
+		fmt.Sprintf("Value length:  %d", e.Length),
+		fmt.Sprintf("Description:   %s", e.Description),
+		fmt.Sprintf("CreateTime:    %s", e.CreateTime.Format(time.RFC3339)),
+		fmt.Sprintf("ModifyTime:    %s", e.ModifyTime.Format(time.RFC3339)),
+	}, "\n")
+}
