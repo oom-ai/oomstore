9a01f63b4b858f54a34210e4aefb5285a1ecac68
Yik San Chan <evan.chanyiksan@gmail.com>
2021-10-18 11:22:37 +0800

    refactor: pass context to WithTransaction

--- internal/database/create_database.go
+++ internal/database/create_database.go
@@ -18,20 +18,20 @@ func CreateDatabase(ctx context.Context, opt Option) (err error) {
 
 	// Use translation to guarantee the following operations be executed
 	// on the same connection: http://go-database-sql.org/modifying.html
-	return db.WithTransaction(func(t *sql.Tx) error {
-		if _, err = t.ExecContext(ctx, fmt.Sprintf("USE `%s`", opt.DbName)); err != nil {
+	return db.WithTransaction(ctx, func(ctx context.Context, tx *sql.Tx) error {
+		if _, err = tx.ExecContext(ctx, fmt.Sprintf("USE `%s`", opt.DbName)); err != nil {
 			return err
 		}
 		// create meta tables
 		for _, schema := range META_TABLE_SCHEMAS {
-			if _, err = t.ExecContext(ctx, schema); err != nil {
+			if _, err = tx.ExecContext(ctx, schema); err != nil {
 				return err
 			}
 		}
 
 		// create meta views
 		for _, schema := range META_VIEW_SCHEMAS {
-			if _, err = t.ExecContext(ctx, schema); err != nil {
+			if _, err = tx.ExecContext(ctx, schema); err != nil {
 				return err
 			}
 		}
