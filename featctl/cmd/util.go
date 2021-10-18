ffb4a8ba9d22f26ea746140a1c4164c146e8fadb
Yik San Chan <evan.chanyiksan@gmail.com>
2021-10-18 20:22:50 +0800

    cleanup: delete deadcode

--- featctl/cmd/util.go
+++ featctl/cmd/util.go
@@ -2,37 +2,11 @@ package cmd
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	"github.com/onestore-ai/onestore/pkg/onestore"
 	"github.com/onestore-ai/onestore/pkg/onestore/types"
 )
-
-var validStatus = []string{
-	"enabled",
-	"disabled",
-}
-
-func contains(slice []string, value string) bool {
-	for _, v := range slice {
-		if v == value {
-			return true
-		}
-	}
-	return false
-}
-
-func validateEnum(name string, value string, possibleValues []string) error {
-	if !contains(possibleValues, value) {
-		return fmt.Errorf("invalid %s: '%s', should be one of %v", name, value, possibleValues)
-	}
-	return nil
-}
-
-func validateStatus(value string) error {
-	return validateEnum("status", value, validStatus)
-}
 
 func mustOpenOneStore(ctx context.Context, opt types.OneStoreOpt) *onestore.OneStore {
 	store, err := onestore.Open(ctx, oneStoreOpt)
