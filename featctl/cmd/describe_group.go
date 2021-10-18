599ec973e748881e5c039e882ddd447a06048979
Jinghan Ying <jinghancc@gmail.com>
2021-10-18 16:03:35 +0800

    feat(featctl): add featctl command describe group

---
+++ featctl/cmd/describe_group.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+var describeGroupCmd = &cobra.Command{
+	Use:   "group",
+	Short: "show details of a specific group",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx := context.Background()
+		oneStore := mustOpenOneStore(ctx, oneStoreOpt)
+		groupName := args[0]
+		group, err := oneStore.GetFeatureGroup(ctx, groupName)
+		if err != nil {
+			log.Fatalf("failed getting group %s, err %v\n", groupName, err)
+		}
+		fmt.Println(group.String())
+	},
+}
+
+func init() {
+	describeCmd.AddCommand(describeGroupCmd)
+}
