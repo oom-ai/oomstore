c86ee88d457cfbbce0420c5bff77ea5d64bf8f56
Wenxuan Zhang <wenxuangm@gmail.com>
2021-10-19 14:46:30 +0800

    feat: implement featctl export command

--- featctl/cmd/export.go
+++ featctl/cmd/export.go
@@ -2,22 +2,33 @@ package cmd
 
 import (
 	"context"
-	"os"
+	"log"
 
 	"github.com/onestore-ai/onestore/featctl/pkg/export"
 	"github.com/spf13/cobra"
 )
 
-var exportOpt export.Option
+var exportOpt export.ExportOpt
 
 var exportCmd = &cobra.Command{
 	Use:   "export",
 	Short: "export a group of features",
-	Args:  cobra.ArbitraryArgs,
+	Args:  cobra.ExactArgs(1),
+	PreRun: func(cmd *cobra.Command, args []string) {
+		if !cmd.Flags().Changed("revision") {
+			exportOpt.GroupRevision = nil
+		}
+		if !cmd.Flags().Changed("limit") {
+			exportOpt.Limit = nil
+		}
+	},
 	Run: func(cmd *cobra.Command, args []string) {
+		exportOpt.GroupName = args[0]
 		ctx := context.Background()
-		exportOpt.DBOption = dbOption
-		export.Export(ctx, &exportOpt)
+		store := mustOpenOneStore(ctx, oneStoreOpt)
+		if err := export.Export(ctx, store, exportOpt); err != nil {
+			log.Fatalf("failed exporting features: %v\n", err)
+		}
 	},
 }
 
@@ -26,9 +37,7 @@ func init() {
 
 	flags := exportCmd.Flags()
 
-	os.Stdout.Name()
-	flags.StringVarP(&exportOpt.Group, "group", "g", "", "feature group")
-	flags.StringSliceVarP(&exportOpt.Features, "name", "n", nil, "feature name")
-	flags.UintVarP(&exportOpt.Limit, "limit", "l", 0, "max records to export")
-	_ = exportCmd.MarkFlagRequired("group")
+	flags.StringSliceVarP(&exportOpt.FeatureNames, "select", "s", nil, "select feature names")
+	exportOpt.Limit = flags.Uint64P("limit", "l", 0, "max records to export")
+	exportOpt.GroupRevision = flags.Int64P("revision", "r", 0, "feature group revision")
 }
