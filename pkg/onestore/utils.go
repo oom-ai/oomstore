5a52020f0f6d9d1ee189097b61e337ebadfed66d
Jinghan Ying <jinghancc@gmail.com>
2021-10-20 14:57:26 +0800

    refactor(pkg/onestore): modify API GetHistoricalFeatureValues

---
+++ pkg/onestore/utils.go
@@ -0,0 +1,19 @@
+package onestore
+
+import (
+	"strconv"
+
+	"github.com/spf13/cast"
+)
+
+func castToInt64(i interface{}) (int64, error) {
+	val := cast.ToInt(i)
+	var err error
+	if bytes, ok := i.([]byte); ok {
+		val, err = strconv.Atoi(string(bytes))
+		if err != nil {
+			return 0, err
+		}
+	}
+	return int64(val), nil
+}
