be4f21a60e5aa167dd72f21cf503773e89683374
Wenxuan Zhang <wenxuangm@gmail.com>
2021-10-21 16:08:16 +0800

    fix: fix featctl register feature

--- internal/database/feature.go
+++ internal/database/feature.go
@@ -13,13 +13,13 @@ import (
 func (db *DB) validateDataType(ctx context.Context, dataType string) error {
 	tmpTableName := fmt.Sprintf("tmp_validate_data_type_%d", rand.Intn(100000))
 	return db.WithTransaction(ctx, func(ctx context.Context, tx *sqlx.Tx) error {
-		if _, err := tx.ExecContext(ctx, fmt.Sprintf("DROP TABLE IF EXISTS `%s`", tmpTableName)); err != nil {
+		if _, err := tx.ExecContext(ctx, fmt.Sprintf("DROP TABLE IF EXISTS %s", tmpTableName)); err != nil {
 			return err
 		}
 		if _, err := tx.ExecContext(ctx, fmt.Sprintf("CREATE TABLE %s (a %s)", tmpTableName, dataType)); err != nil {
 			return err
 		}
-		if _, err := tx.ExecContext(ctx, fmt.Sprintf("DROP TABLE `%s`", tmpTableName)); err != nil {
+		if _, err := tx.ExecContext(ctx, fmt.Sprintf("DROP TABLE %s", tmpTableName)); err != nil {
 			return err
 		}
 		return nil
@@ -30,7 +30,7 @@ func (db *DB) CreateFeature(ctx context.Context, opt types.CreateFeatureOpt) err
 	if err := db.validateDataType(ctx, opt.ValueType); err != nil {
 		return fmt.Errorf("err when validating value_type input, details: %s", err.Error())
 	}
-	query := "INSERT INTO feature(name, group_name, value_type, description) VALUES (?, ?, ?, ?)"
+	query := "INSERT INTO feature(name, group_name, value_type, description) VALUES ($1, $2, $3, $4)"
 	_, err := db.ExecContext(ctx, query, opt.FeatureName, opt.GroupName, opt.ValueType, opt.Description)
 	if err != nil {
 		if errNumber := GetErrNumber(err); errNumber == ER_DUP_ENTRY {
@@ -42,7 +42,7 @@ func (db *DB) CreateFeature(ctx context.Context, opt types.CreateFeatureOpt) err
 
 func (db *DB) GetFeature(ctx context.Context, featureName string) (*types.Feature, error) {
 	var feature types.Feature
-	query := `SELECT * FROM feature WHERE name = ?`
+	query := `SELECT * FROM feature WHERE name = $1`
 	if err := db.GetContext(ctx, &feature, query, featureName); err != nil {
 		return nil, err
 	}
