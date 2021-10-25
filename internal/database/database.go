390ce65f2d66df0b44d6c9ec09154f2f5b925780
lianxmfor <1034552569@qq.com>
2021-10-25 20:06:52 +0800

    feat(database): remove useless code

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,8 +1,6 @@
 package database
 
 import (
-	"context"
-	"database/sql"
 	"fmt"
 
 	"github.com/jmoiron/sqlx"
@@ -39,36 +37,4 @@ func OpenWith(host, port, user, pass, dbName string) (*DB, error) {
 			dbName),
 	)
 	return &DB{db}, err
-}
-
-type Column struct {
-	Field   string
-	Type    string
-	Null    string
-	Key     string
-	Default *string
-	Extra   string
-}
-
-func (db *DB) ColumnInfo(ctx context.Context, table string, column string) (Column, error) {
-	var result Column
-	err := db.QueryRowContext(ctx, fmt.Sprintf(`show columns from "%s" like "%s"`, table, column)).
-		Scan(&result.Field, &result.Type, &result.Null, &result.Key, &result.Default, &result.Extra)
-	if err == sql.ErrNoRows {
-		return result, fmt.Errorf(`column "%s" not found in table "%s"`, column, table)
-	}
-	return result, err
-}
-
-func (db *DB) TableExists(ctx context.Context, table string) (bool, error) {
-	var result string
-	err := db.GetContext(ctx, &result, fmt.Sprintf(`show tables like "%s"`, table))
-	switch {
-	case err == sql.ErrNoRows:
-		return false, nil
-	case err != nil:
-		return false, err
-	default:
-		return true, nil
-	}
 }
