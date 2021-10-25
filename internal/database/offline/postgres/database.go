d98835d5228387c57233d684f25b4e1e77bc413f
lianxmfor <1034552569@qq.com>
2021-10-25 16:12:16 +0800

    feat(offline store): add open function

--- internal/database/offline/postgres/database.go
+++ internal/database/offline/postgres/database.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 
 	"github.com/jmoiron/sqlx"
+	"github.com/onestore-ai/onestore/pkg/onestore/types"
 )
 
 type DB struct {
 	*sqlx.DB
+}
+
+func Open(option *types.PostgresDbOpt) (*DB, error) {
+	return OpenWith(option.Host, option.Port, option.User, option.Pass, option.Database)
 }
 
 func OpenWith(host, port, user, pass, database string) (*DB, error) {
