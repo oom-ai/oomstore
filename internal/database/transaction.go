1fc670b2def3a7bd164daf7c8fc90913985aabd7
Jinghan Ying <jinghancc@gmail.com>
2021-10-25 20:05:54 +0800

    refactor(internal/database): del receiver method WithTransaction

--- internal/database/transaction.go
+++ internal/database/transaction.go
@@ -12,31 +12,6 @@ import (
 // that can be used for executing statements and queries against a database.
 type TxFn func(ctx context.Context, tx *sqlx.Tx) error
 
-// WithTransaction creates a new transaction and handles rollback/commit based on the
-// error object returned by the `TxFn`
-func (db *DB) WithTransaction(ctx context.Context, fn TxFn) (err error) {
-	tx, err := db.BeginTxx(ctx, nil)
-	if err != nil {
-		return
-	}
-
-	defer func() {
-		if p := recover(); p != nil {
-			// a panic occurred, rollback and repanic
-			_ = tx.Rollback()
-			panic(p)
-		} else if err != nil {
-			// something went wrong, rollback
-			_ = tx.Rollback()
-		} else {
-			// all good, commit
-			err = tx.Commit()
-		}
-	}()
-
-	return fn(ctx, tx)
-}
-
 func WithTransaction(db *sqlx.DB, ctx context.Context, fn TxFn) (err error) {
 	tx, err := db.BeginTxx(ctx, nil)
 	if err != nil {
