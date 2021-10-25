f6309ba2d9b2f41ffe705c7028659e11d8f26e90
Jinghan Ying <jinghancc@gmail.com>
2021-10-25 16:44:05 +0800

    feaet(internal/database): add new func WithTransaction The new func is different from the receiver method WithTransaction, which takes in sqlx.DB as a parameter.

--- internal/database/transaction.go
+++ internal/database/transaction.go
@@ -36,3 +36,26 @@ func (db *DB) WithTransaction(ctx context.Context, fn TxFn) (err error) {
 
 	return fn(ctx, tx)
 }
+
+func WithTransaction(db *sqlx.DB, ctx context.Context, fn TxFn) (err error) {
+	tx, err := db.BeginTxx(ctx, nil)
+	if err != nil {
+		return
+	}
+
+	defer func() {
+		if p := recover(); p != nil {
+			// a panic occurred, rollback and repanic
+			_ = tx.Rollback()
+			panic(p)
+		} else if err != nil {
+			// something went wrong, rollback
+			_ = tx.Rollback()
+		} else {
+			// all good, commit
+			err = tx.Commit()
+		}
+	}()
+
+	return fn(ctx, tx)
+}
