fb80933df2d2678f6dfcbbf0f94137ae51ef2a0a
Wenxuan Zhang <wenxuangm@gmail.com>
2021-10-26 18:57:50 +0800

    test: add test for `ValueTypeTag`

---
+++ internal/database/offline/postgres/value_type_test.go
@@ -0,0 +1,40 @@
+package postgres
+
+import (
+	"testing"
+
+	"github.com/onestore-ai/onestore/pkg/onestore/types"
+)
+
+func TestValueTypeTag(t *testing.T) {
+	for _, tt := range []struct {
+		input    string
+		expected string
+	}{
+		{
+			"bit varying",
+			types.BYTE_ARRAY,
+		},
+		{
+			"int",
+			types.INT32,
+		},
+		{
+			"varchar(32)",
+			types.STRING,
+		},
+		{
+			"timestamp without time zone",
+			types.TIME,
+		},
+	} {
+		actual, err := ValueTypeTag(tt.input)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if actual != tt.expected {
+			t.Errorf("expected %s, got %s", tt.expected, actual)
+		}
+	}
+}
