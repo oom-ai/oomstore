c5ee85459a82f23cd96ff6de96ac842fddc477e1
Jinghan Ying <jinghancc@gmail.com>
2021-10-26 19:55:37 +0800

    refactor(internal/database): refactor SinkFeatureValuesStream Do not drop previous revision table after sinking the new revision table.

--- pkg/onestore/types/types.go
+++ pkg/onestore/types/types.go
@@ -65,6 +65,10 @@ type Revision struct {
 	ModifyTime  time.Time `db:"modify_time"`
 }
 
+func (r *Revision) GetOnlineBatchTableName() string {
+	return fmt.Sprintf("batch_%d", r.ID)
+}
+
 func (rf *RichFeature) ToFeature() *Feature {
 	if rf == nil {
 		return nil
