61e747d4291199f93d5ec1805da38673bccc5d4f
lianxmfor <1034552569@qq.com>
2021-10-26 21:35:23 +0800

    feat(metastore/postres): add CreateDatabase unit test

--- internal/database/metadata/postgres/database_test.go
+++ internal/database/metadata/postgres/database_test.go
@@ -2,7 +2,10 @@ package postgres
 
 import (
 	"context"
+	"sort"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 
 	"github.com/onestore-ai/onestore/internal/database/test"
 	"github.com/onestore-ai/onestore/pkg/onestore/types"
@@ -29,4 +32,42 @@ func initDB(t *testing.T) {
 	if err := CreateDatabase(context.Background(), test.PostgresDbopt); err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestCreateDatabase(t *testing.T) {
+	ctx := context.Background()
+	if err := CreateDatabase(ctx, test.PostgresDbopt); err != nil {
+		t.Fatal(err)
+	}
+
+	store, err := Open(&test.PostgresDbopt)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer store.Close()
+
+	var tables []string
+	if err = store.SelectContext(ctx, &tables,
+		`SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'public'
+ORDER BY table_name;`); err != nil {
+		t.Fatal(err)
+	}
+
+	var wantTables []string
+	for table := range META_TABLE_SCHEMAS {
+		wantTables = append(wantTables, table)
+	}
+	for table := range META_VIEW_SCHEMAS {
+		wantTables = append(wantTables, table)
+	}
+
+	sort.Slice(tables, func(i, j int) bool {
+		return tables[i] < tables[j]
+	})
+	sort.Slice(wantTables, func(i, j int) bool {
+		return wantTables[i] < wantTables[j]
+	})
+	assert.Equal(t, wantTables, tables)
 }
