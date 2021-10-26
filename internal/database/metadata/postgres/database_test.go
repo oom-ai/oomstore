cd7e5e9b331b3d91a5f348db97ca1b26e3557473
lianxmfor <1034552569@qq.com>
2021-10-26 21:37:21 +0800

    feat(metastore/postgres): add entity methods unit test

--- internal/database/metadata/postgres/database_test.go
+++ internal/database/metadata/postgres/database_test.go
@@ -71,3 +71,79 @@ ORDER BY table_name;`); err != nil {
 	})
 	assert.Equal(t, wantTables, tables)
 }
+
+func TestEntity(t *testing.T) {
+	initDB(t)
+
+	store, err := Open(&test.PostgresDbopt)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer store.Close()
+
+	deviceEntity := types.Entity{
+		Name:        "device",
+		Length:      32,
+		Description: "description",
+	}
+	userEntity := types.Entity{
+		Name:        "user",
+		Length:      64,
+		Description: "description",
+	}
+
+	// test CreateEntity
+	{
+		if err := store.CreateEntity(context.Background(), types.CreateEntityOpt{
+			Name:        deviceEntity.Name,
+			Length:      deviceEntity.Length,
+			Description: deviceEntity.Description,
+		}); err != nil {
+			t.Error(err)
+		}
+
+		if err := store.CreateEntity(context.Background(), types.CreateEntityOpt{
+			Name:        userEntity.Name,
+			Length:      userEntity.Length,
+			Description: userEntity.Description,
+		}); err != nil {
+			t.Error(err)
+		}
+	}
+
+	// test GetEntity
+	{
+		entity, err := store.GetEntity(context.Background(), "device")
+		if err != nil {
+			t.Error(err)
+		}
+		assert.Equal(t, deviceEntity.Name, entity.Name)
+		assert.Equal(t, deviceEntity.Length, entity.Length)
+		assert.Equal(t, deviceEntity.Description, entity.Description)
+	}
+
+	// test UpdateEntity
+	{
+		if err := store.UpdateEntity(context.Background(), types.UpdateEntityOpt{
+			EntityName:     "user",
+			NewDescription: "new description",
+		}); err != nil {
+			t.Error(err)
+		}
+
+		entity, err := store.GetEntity(context.Background(), "user")
+		if err != nil {
+			t.Error(err)
+		}
+		assert.Equal(t, "new description", entity.Description)
+	}
+
+	// test ListEntity
+	{
+		entitys, err := store.ListEntity(context.Background())
+		if err != nil {
+			t.Error(err)
+		}
+		assert.Equal(t, 2, len(entitys))
+	}
+}
