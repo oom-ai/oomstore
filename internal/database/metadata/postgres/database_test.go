928ef31da8f2f2389684d22b513b772504e38e75
lianxmfor <1034552569@qq.com>
2021-10-26 16:36:55 +0800

    feat(metastore): add gnomock for unit test

---
+++ internal/database/metadata/postgres/database_test.go
@@ -0,0 +1,32 @@
+package postgres
+
+import (
+	"context"
+	"testing"
+
+	"github.com/onestore-ai/onestore/internal/database/test"
+	"github.com/onestore-ai/onestore/pkg/onestore/types"
+)
+
+func initDB(t *testing.T) {
+	opt := test.PostgresDbopt
+	store, err := Open(&types.PostgresDbOpt{
+		Host:     opt.Host,
+		Port:     opt.Port,
+		User:     opt.User,
+		Pass:     opt.Pass,
+		Database: "test",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := store.ExecContext(context.Background(), "drop database if exists onestore"); err != nil {
+		t.Fatal(err)
+	}
+	store.Close()
+
+	if err := CreateDatabase(context.Background(), test.PostgresDbopt); err != nil {
+		t.Fatal(err)
+	}
+}
---
+++ internal/database/test/postgres.go
@@ -0,0 +1,41 @@
+package test
+
+import (
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+
+	"github.com/onestore-ai/onestore/pkg/onestore/types"
+	"github.com/orlangure/gnomock"
+	"github.com/orlangure/gnomock/preset/postgres"
+)
+
+var PostgresDbopt types.PostgresDbOpt
+
+func init() {
+	postgresContainer, err := gnomock.Start(postgres.Preset(
+		postgres.WithUser("test", "test"),
+		postgres.WithDatabase("test"),
+		postgres.WithTimezone("Asia/Shanghai"),
+	))
+	if err != nil {
+		panic(err)
+	}
+
+	PostgresDbopt = types.PostgresDbOpt{
+		Host:     postgresContainer.Host,
+		Port:     strconv.Itoa(postgresContainer.DefaultPort()),
+		User:     "test",
+		Pass:     "test",
+		Database: "onestore",
+	}
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGINT, syscall.SIGQUIT)
+		<-c
+
+		_ = gnomock.Stop(postgresContainer)
+	}()
+}
