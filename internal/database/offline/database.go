6d597e1badbe920be684e6b221d9a335847381c5
Wenxuan Zhang <wenxuangm@gmail.com>
2021-10-27 16:53:32 +0800

    refactor: re-organize offline store api options

--- internal/database/offline/database.go
+++ internal/database/offline/database.go
@@ -9,9 +9,9 @@ import (
 )
 
 type Store interface {
-	GetPointInTimeFeatureValues(ctx context.Context, entity *types.Entity, entityRows []types.EntityRow, revisionRanges []*types.RevisionRange, features []*types.RichFeature) (dataMap map[string]dbutil.RowMap, err error)
+	GetPointInTimeFeatureValues(ctx context.Context, opt GetPointInTimeFeatureValuesOpt) (map[string]dbutil.RowMap, error)
 	GetFeatureValuesStream(ctx context.Context, opt GetFeatureValuesStreamOpt) (<-chan *types.RawFeatureValueRecord, error)
-	ImportBatchFeatures(ctx context.Context, opt types.ImportBatchFeaturesOpt, entity *types.Entity, features []*types.Feature, header []string) (int64, string, error)
+	ImportBatchFeatures(ctx context.Context, opt ImportBatchFeaturesOpt) (int64, string, error)
 
 	ValueTypeTag(dbValueType string) (string, error)
 	io.Closer
