f6884c861836018d186db1ae203c502a875a601d
Wenxuan Zhang <wenxuangm@gmail.com>
2021-10-27 15:27:06 +0800

    refactor: re-organize metadata store

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,12 +1,17 @@
 package database
 
 import (
+	"context"
 	"fmt"
+
+	"github.com/onestore-ai/onestore/pkg/onestore/types"
+
+	"github.com/onestore-ai/onestore/internal/database/metadata"
+	metadataPg "github.com/onestore-ai/onestore/internal/database/metadata/postgres"
 
 	"github.com/onestore-ai/onestore/internal/database/online"
 	onlinePG "github.com/onestore-ai/onestore/internal/database/online/postgres"
 	onlineRedis "github.com/onestore-ai/onestore/internal/database/online/redis"
-	"github.com/onestore-ai/onestore/pkg/onestore/types"
 )
 
 func OpenOnlineStore(opt types.OnlineStoreOpt) (online.Store, error) {
@@ -17,5 +22,23 @@ func OpenOnlineStore(opt types.OnlineStoreOpt) (online.Store, error) {
 		return onlineRedis.Open(opt.RedisDbOpt), nil
 	default:
 		return nil, fmt.Errorf("unsupported backend: %s", opt.Backend)
+	}
+}
+
+func OpenMetadataStore(opt types.MetaStoreOpt) (metadata.Store, error) {
+	switch opt.Backend {
+	case types.POSTGRES:
+		return metadataPg.Open(opt.PostgresDbOpt)
+	default:
+		return nil, fmt.Errorf("unsupported backend: %s", opt.Backend)
+	}
+}
+
+func CreateMetadataDatabase(ctx context.Context, opt types.MetaStoreOpt) error {
+	switch opt.Backend {
+	case types.POSTGRES:
+		return metadataPg.CreateDatabase(ctx, *opt.PostgresDbOpt)
+	default:
+		return fmt.Errorf("unsupported backend: %s", opt.Backend)
 	}
 }
