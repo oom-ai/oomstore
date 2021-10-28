21bef0572745c78d12735212df7eb908724d6b4c
lianxmfor <1034552569@qq.com>
2021-10-28 17:35:46 +0800

    feat(metastore/postgres): fix entity for unit test

--- internal/database/metadata/postgres/entity.go
+++ internal/database/metadata/postgres/entity.go
@@ -2,13 +2,21 @@ package postgres
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/jackc/pgerrcode"
+	"github.com/lib/pq"
 	"github.com/oom-ai/oomstore/pkg/oomstore/types"
 )
 
 func (db *DB) CreateEntity(ctx context.Context, opt types.CreateEntityOpt) error {
 	query := "insert into feature_entity(name, length, description) values($1, $2, $3)"
 	_, err := db.ExecContext(ctx, query, opt.Name, opt.Length, opt.Description)
+	if er, ok := err.(*pq.Error); ok {
+		if er.Code == pgerrcode.UniqueViolation {
+			return fmt.Errorf("entity %s already exist!", opt.Name)
+		}
+	}
 	return err
 }
 
