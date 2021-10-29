613dbf141da83bdd2675b401958a11830578e7b9
lianxmfor <1034552569@qq.com>
2021-10-29 14:20:48 +0800

    test(metastore/postgres): split feature group unit test

--- internal/database/metadata/postgres/database_test.go
+++ internal/database/metadata/postgres/database_test.go
@@ -340,82 +340,103 @@ func TestUpdateFeatuer(t *testing.T) {
 	assert.Equal(t, "new description", feature.Description)
 }
 
-func TestFeatureGroup(t *testing.T) {
-	initDB(t)
-
-	store, err := Open(&test.PostgresDbopt)
-	assert.Nil(t, err)
-	defer store.Close()
-
-	assert.Nil(t, store.CreateEntity(context.Background(), types.CreateEntityOpt{
-		Name:        "device",
-		Length:      32,
-		Description: "description",
-	}))
-
-	baseInfoFg := metadata.CreateFeatureGroupOpt{
-		CreateFeatureGroupOpt: types.CreateFeatureGroupOpt{
-			Name:        "deviec_baseinfo",
-			EntityName:  "device",
-			Description: "description",
-		},
-		Category: types.BatchFeatureCategory,
-	}
+func TestCreateFeatureGroup(t *testing.T) {
+	db := initAndOpenDB(t)
+	defer db.Close()
 
 	infoFg := metadata.CreateFeatureGroupOpt{
 		CreateFeatureGroupOpt: types.CreateFeatureGroupOpt{
-			Name:        "deviec_info",
+			Name:        "device_info",
 			EntityName:  "device",
 			Description: "description",
 		},
 		Category: types.BatchFeatureCategory,
 	}
 
-	// test CreateFeatureGroup
-	{
-		errFg := baseInfoFg
-		errFg.Category = "invalid-category"
-		assert.NotNil(t, store.CreateFeatureGroup(context.Background(), errFg))
-		assert.Nil(t, store.CreateFeatureGroup(context.Background(), baseInfoFg))
-		assert.Nil(t, store.CreateFeatureGroup(context.Background(), infoFg))
+	assert.Nil(t, db.CreateFeatureGroup(context.Background(), infoFg))
+	assert.Equal(t, db.CreateFeatureGroup(context.Background(), infoFg), fmt.Errorf("feature group device_info already exist"))
+
+	errInfoFg := infoFg
+	errInfoFg.Category = "invalid-category"
+	assert.NotNil(t, db.CreateFeatureGroup(context.Background(), errInfoFg))
+}
+
+func TestGetFeatureGroup(t *testing.T) {
+	db := initAndOpenDB(t)
+	defer db.Close()
+
+	fg, err := db.GetFeatureGroup(context.Background(), "invalid-feature-group")
+	assert.NotNil(t, err)
+	assert.Nil(t, fg)
+
+	infoFg := metadata.CreateFeatureGroupOpt{
+		CreateFeatureGroupOpt: types.CreateFeatureGroupOpt{
+			Name:        "device_info",
+			EntityName:  "device",
+			Description: "description",
+		},
+		Category: types.BatchFeatureCategory,
 	}
+	assert.Nil(t, db.CreateFeatureGroup(context.Background(), infoFg))
 
-	// test GetFeatureGroup
-	{
-		fg, err := store.GetFeatureGroup(context.Background(), baseInfoFg.Name)
-		assert.Nil(t, err)
+	fg, err = db.GetFeatureGroup(context.Background(), "device_info")
+	assert.Nil(t, err)
+	assert.Equal(t, infoFg.Category, fg.Category)
+	assert.Equal(t, infoFg.EntityName, fg.EntityName)
+	assert.Equal(t, infoFg.Description, fg.Description)
+	assert.Equal(t, infoFg.Category, fg.Category)
 
-		assert.Equal(t, baseInfoFg.Category, fg.Category)
-		assert.Equal(t, baseInfoFg.EntityName, fg.EntityName)
-		assert.Equal(t, baseInfoFg.Description, fg.Description)
-		assert.Equal(t, baseInfoFg.Category, fg.Category)
-	}
+	fg, err = db.GetFeatureGroup(context.Background(), "invalid-feature-group")
+	assert.NotNil(t, err)
+	assert.Nil(t, fg)
+}
 
-	// test UpdateFeatureGroup
-	{
-		description := "new description"
-		revisionId := int32(2)
-		assert.Nil(t, store.UpdateFeatureGroup(context.Background(), types.UpdateFeatureGroupOpt{
-			GroupName:        baseInfoFg.Name,
-			Description:      &description,
-			OnlineRevisionId: &revisionId,
-		}))
+func TestUpdateFeatureGroup(t *testing.T) {
+	db := initAndOpenDB(t)
+	defer db.Close()
 
-		fg, err := store.GetFeatureGroup(context.Background(), baseInfoFg.Name)
-		assert.Nil(t, err)
+	rowsAffected, err := db.UpdateFeatureGroup(context.Background(), types.UpdateFeatureGroupOpt{
+		GroupName: "invalid-group",
+	})
+	assert.NotNil(t, err)
+	assert.Equal(t, int64(0), rowsAffected)
 
-		assert.Equal(t, "new description", fg.Description)
-		assert.Equal(t, revisionId, *fg.OnlineRevisionID)
-	}
+	description := "new description"
+	rowsAffected, err = db.UpdateFeatureGroup(context.Background(), types.UpdateFeatureGroupOpt{
+		GroupName:   "invalid-group",
+		Description: &description,
+	})
+	assert.Nil(t, err)
+	assert.Equal(t, int64(0), rowsAffected)
+}
 
-	// test ListFeatureGroup
-	{
-		entityName := "device"
-		fgs, err := store.ListFeatureGroup(context.Background(), &entityName)
-		assert.Nil(t, err)
+func TestListFeatureGroup(t *testing.T) {
+	db := initAndOpenDB(t)
+	defer db.Close()
 
-		assert.Equal(t, 2, len(fgs))
-	}
+	entityName := "invalid-entity-name"
+	fgs, err := db.ListFeatureGroup(context.Background(), &entityName)
+	assert.Nil(t, err)
+	assert.Equal(t, 0, len(fgs))
+
+	assert.Nil(t, db.CreateFeatureGroup(context.Background(), metadata.CreateFeatureGroupOpt{
+		CreateFeatureGroupOpt: types.CreateFeatureGroupOpt{
+			Name:        "device_baseinfo",
+			EntityName:  "device",
+			Description: "description",
+		},
+		Category: types.BatchFeatureCategory,
+	}))
+
+	entityName = "device"
+	fgs, err = db.ListFeatureGroup(context.Background(), &entityName)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(fgs))
+
+	entityName = "invalid_entity_name"
+	fgs, err = db.ListFeatureGroup(context.Background(), &entityName)
+	assert.Nil(t, err)
+	assert.Equal(t, 0, len(fgs))
 }
 
 func TestRevision(t *testing.T) {
