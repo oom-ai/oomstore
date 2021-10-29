2fbf3b55ffdd8927a3d5cb8020b075240bf888ea
Wenxuan Zhang <wenxuangm@gmail.com>
2021-10-29 10:23:16 +0800

    feat: add oom config for featctl

--- featctl/cmd/root.go
+++ featctl/cmd/root.go
@@ -2,33 +2,31 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/adrg/xdg"
 	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
-	"github.com/spf13/viper"
+	"gopkg.in/yaml.v2"
 
 	"github.com/oom-ai/oomstore/pkg/oomstore/types"
 	"github.com/oom-ai/oomstore/version"
 )
 
-const (
-	envPrefix = "FEATCTL"
-)
-
 var cfgFile string
 var defaultCfgFile = filepath.Join(xdg.ConfigHome, "featctl", "config.yaml")
 
-var oomStoreOpt types.OomStoreOpt
+var oomStoreOpt types.OomStoreOptV2
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:     "featctl",
-	Short:   "a cli tool lets you control the feature store.",
+	Short:   "a cli tool lets you control the oom feature store.",
 	Version: version.String(),
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		// panic("sjdfiod")
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -49,47 +47,20 @@ func init() {
 
 	pFlags := rootCmd.PersistentFlags()
 	pFlags.StringVar(&cfgFile, "config", defaultCfgFile, "config file")
-
-	pFlags.StringVarP(&oomStoreOpt.Host, "host", "H", "127.0.0.1", "feature store database host")
-	pFlags.StringVarP(&oomStoreOpt.Port, "port", "P", "5432", "feature store database port")
-	pFlags.StringVarP(&oomStoreOpt.User, "user", "u", "root", "feature store database user")
-	pFlags.StringVarP(&oomStoreOpt.Pass, "pass", "p", "", "feature store database pass")
-	pFlags.StringVarP(&oomStoreOpt.Workspace, "workspace", "w", "oomstore", "feature store workspace name")
-
 	rootCmd.SetVersionTemplate(`{{printf "%s\n" .Version}}`)
 }
 
-// initConfig reads in config file and ENV variables if set.
+// initConfig reads in config file
 func initConfig() {
-	viper.SetConfigFile(cfgFile)
-	viper.SetEnvPrefix(envPrefix)
-	viper.AutomaticEnv() // read in environment variables that match
-	// If a config file is found, read it in.
-	_ = viper.ReadInConfig()
-	bindViperFlags(rootCmd)
-}
+	if envCfgFile := os.Getenv("FEATCTL_CONFIG"); envCfgFile != "" {
+		cfgFile = envCfgFile
+	}
+	cfgContent, err := ioutil.ReadFile(cfgFile)
+	if err != nil {
+		panic(fmt.Sprintf("failed reading config file: %v\n", err))
+	}
 
-// Bind each cobra flag to its associated viper configuration (config file and environment variable)
-func bindViperFlags(cmd *cobra.Command) {
-	cmd.Flags().VisitAll(func(f *pflag.Flag) {
-		// Environment variables can't have dashes in them, so bind them to their equivalent
-		// keys with underscores, e.g. --favorite-color to STING_FAVORITE_COLOR
-		if strings.Contains(f.Name, "-") {
-			envVarSuffix := strings.ToUpper(strings.ReplaceAll(f.Name, "-", "_"))
-			envName := fmt.Sprintf("%s_%s", envPrefix, envVarSuffix)
-			if err := viper.BindEnv(f.Name, envName); err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-		}
-
-		// Apply the viper config value to the flag when the flag is not set and viper has a value
-		if !f.Changed && viper.IsSet(f.Name) {
-			val := viper.Get(f.Name)
-			if err := cmd.Flags().Set(f.Name, fmt.Sprintf("%v", val)); err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-		}
-	})
+	if err := yaml.Unmarshal(cfgContent, &oomStoreOpt); err != nil {
+		panic(fmt.Sprintf("failed loading config : %v\n", err))
+	}
 }
--- pkg/oomstore/types/config.go
+++ pkg/oomstore/types/config.go
@@ -8,38 +8,38 @@ const (
 )
 
 type OomStoreOptV2 struct {
-	MetaStoreOpt    MetaStoreOpt
-	OfflineStoreOpt OfflineStoreOpt
-	OnlineStoreOpt  OnlineStoreOpt
+	MetaStoreOpt    MetaStoreOpt    `yaml:"meta-store"`
+	OfflineStoreOpt OfflineStoreOpt `yaml:"offline-store"`
+	OnlineStoreOpt  OnlineStoreOpt  `yaml:"online-store"`
 }
 
 type OnlineStoreOpt struct {
-	Backend       BackendType
-	PostgresDbOpt *PostgresDbOpt
-	RedisDbOpt    *RedisDbOpt
+	Backend       BackendType    `yaml:"backend"`
+	PostgresDbOpt *PostgresDbOpt `yaml:"postgres"`
+	RedisDbOpt    *RedisDbOpt    `yaml:"redis"`
 }
 
 type OfflineStoreOpt struct {
-	Backend       BackendType
-	PostgresDbOpt *PostgresDbOpt
+	Backend       BackendType    `yaml:"backend"`
+	PostgresDbOpt *PostgresDbOpt `yaml:"postgres"`
 }
 
 type MetaStoreOpt struct {
-	Backend       BackendType
-	PostgresDbOpt *PostgresDbOpt
+	Backend       BackendType    `yaml:"backend"`
+	PostgresDbOpt *PostgresDbOpt `yaml:"postgres"`
 }
 
 type RedisDbOpt struct {
-	Host     string
-	Port     string
-	Pass     string
-	Database int
+	Host     string `yaml:"host"`
+	Port     string `yaml:"port"`
+	Pass     string `yaml:"password"`
+	Database int    `yaml:"database"`
 }
 
 type PostgresDbOpt struct {
-	Host     string
-	Port     string
-	User     string
-	Pass     string
-	Database string
+	Host     string `yaml:"host"`
+	Port     string `yaml:"port"`
+	User     string `yaml:"user"`
+	Pass     string `yaml:"password"`
+	Database string `yaml:"database"`
 }
