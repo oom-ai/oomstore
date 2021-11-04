2507715ee1ded06b81e5b31a5dfdb68b73514b2b
Jinghan Ying <jinghancc@gmail.com>
2021-11-04 11:09:08 +0800

    feat(featctl/cmd): add format for command list entity

--- featctl/cmd/util.go
+++ featctl/cmd/util.go
@@ -8,10 +8,19 @@ import (
 	"github.com/oom-ai/oomstore/pkg/oomstore/types"
 )
 
+const (
+	CSV        = "csv"
+	ASCIITable = "ascii_table"
+)
+
 func mustOpenOomStore(ctx context.Context, opt types.OomStoreConfig) *oomstore.OomStore {
 	store, err := oomstore.Open(ctx, oomStoreCfg)
 	if err != nil {
 		log.Fatalf("failed opening OomStore: %v", err)
 	}
 	return store
+}
+
+func stringPtr(s string) *string {
+	return &s
 }
--- featctl/test/test_list_entity.sh
+++ featctl/test/test_list_entity.sh
@@ -11,6 +11,6 @@ expected='Name,Length,Description,CreateTime,ModifyTime
 device,32,,2021-10-19T06:56:07Z,2021-10-19T06:56:07Z
 user,64,all users,2021-10-19T06:56:07Z,2021-10-19T06:56:07Z
 '
-actual=$(featctl list entity)
+actual=$(featctl list entity -o csv)
 ignore_time() { cut -d ',' -f 1-3 <<<"$1"; }
 assert_eq "$case" "$(ignore_time "$expected" | sort)" "$(ignore_time "$actual" | sort)"
