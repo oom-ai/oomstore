8259a6679c11a618df3b47e6fa23f65fa3392576
lianxmfor <1034552569@qq.com>
2021-11-05 17:43:29 +0800

    refactor(featctl): put the output option of listxxx command into the list command

--- featctl/cmd/list.go
+++ featctl/cmd/list.go
@@ -4,6 +4,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var listOutput *string
+
 var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "list resources",
@@ -11,4 +13,7 @@ var listCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+
+	flags := listCmd.PersistentFlags()
+	listOutput = flags.StringP("output", "o", ASCIITable, "output format csv [csv,ascii_table]")
 }
