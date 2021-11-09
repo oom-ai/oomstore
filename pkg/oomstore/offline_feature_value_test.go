196f4f190e2b1cc1f5ef07fa9662b477331dce92
Jinghan Ying <jinghancc@gmail.com>
2021-11-09 15:52:40 +0800

    test(pkg/oomstore): modify unit test TestGetHistoricalFeatureValues

--- pkg/oomstore/offline_feature_value_test.go
+++ pkg/oomstore/offline_feature_value_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/oom-ai/oomstore/internal/database/dbutil"
 	"github.com/oom-ai/oomstore/internal/database/metadata/mock_metadata"
-	"github.com/oom-ai/oomstore/internal/database/offline"
 	"github.com/oom-ai/oomstore/internal/database/offline/mock_offline"
 	"github.com/oom-ai/oomstore/pkg/oomstore"
 	"github.com/oom-ai/oomstore/pkg/oomstore/types"
@@ -37,7 +36,9 @@ func TestGetHistoricalFeatureValues(t *testing.T) {
 			DataTable:   "device_basic_1",
 		},
 	}
-	entityRows, joinResult, result := prepareEntityRowsAndResult()
+
+	joined, result := prepareResults()
+	entityRows := make(chan types.EntityRow)
 
 	testCases := []struct {
 		description   string
@@ -45,7 +46,7 @@ func TestGetHistoricalFeatureValues(t *testing.T) {
 		features      types.FeatureList
 		entity        *types.Entity
 		featureMap    map[string]types.FeatureList
-		joinResultMap map[string]map[string]dbutil.RowMap
+		joined        <-chan dbutil.RowMapRecord
 		expectedError error
 		expected      []*types.EntityRowWithFeatures
 	}{
@@ -70,6 +71,22 @@ func TestGetHistoricalFeatureValues(t *testing.T) {
 			expected:      nil,
 		},
 		{
+			description: "nil joined, return nil",
+			opt: types.GetHistoricalFeatureValuesOpt{
+				FeatureNames: consistentFeatures.Names(),
+				EntityRows:   entityRows,
+			},
+			entity:   &entity,
+			features: consistentFeatures,
+			featureMap: map[string]types.FeatureList{
+				"device_basic":    {consistentFeatures[0]},
+				"device_advanced": {consistentFeatures[1]},
+			},
+			joined:        nil,
+			expectedError: nil,
+			expected:      nil,
+		},
+		{
 			description: "consistent entity type, succeed",
 			opt: types.GetHistoricalFeatureValuesOpt{
 				FeatureNames: consistentFeatures.Names(),
@@ -81,7 +98,7 @@ func TestGetHistoricalFeatureValues(t *testing.T) {
 				"device_basic":    {consistentFeatures[0]},
 				"device_advanced": {consistentFeatures[1]},
 			},
-			joinResultMap: joinResult,
+			joined:        joined,
 			expectedError: nil,
 			expected:      result,
 		},
@@ -92,83 +109,60 @@ func TestGetHistoricalFeatureValues(t *testing.T) {
 			metadataStore.EXPECT().ListFeature(gomock.Any(), types.ListFeatureOpt{FeatureNames: tc.opt.FeatureNames}).Return(tc.features, nil)
 			if tc.entity != nil {
 				metadataStore.EXPECT().GetEntity(gomock.Any(), tc.entity.Name).Return(&entity, nil)
-				for groupName, featureSlice := range tc.featureMap {
+				for groupName := range tc.featureMap {
 					metadataStore.EXPECT().BuildRevisionRanges(gomock.Any(), groupName).Return(revisionRanges, nil).AnyTimes()
-					offlineStore.EXPECT().Join(gomock.Any(), offline.JoinOpt{
-						Entity:         &entity,
-						EntityRows:     tc.opt.EntityRows,
-						RevisionRanges: revisionRanges,
-						Features:       featureSlice,
-					}).Return(tc.joinResultMap[groupName], nil)
 				}
+				offlineStore.EXPECT().Join(gomock.Any(), gomock.Any()).Return(tc.joined, nil)
 			}
 
 			actual, err := store.GetHistoricalFeatureValues(context.Background(), tc.opt)
 			if tc.expectedError != nil {
 				assert.Error(t, err, tc.expectedError)
+			} else if tc.expected == nil {
+				var emptyChannel <-chan *types.EntityRowWithFeatures
+				assert.Equal(t, emptyChannel, actual)
 			} else {
 				assert.NoError(t, err)
-				assert.Equal(t, tc.expected, actual)
+				values := make([]*types.EntityRowWithFeatures, 0)
+				for item := range actual {
+					values = append(values, item)
+				}
+				assert.Equal(t, tc.expected, values)
 			}
 		})
 	}
 }
 
-func prepareEntityRowsAndResult() ([]types.EntityRow, map[string]map[string]dbutil.RowMap, []*types.EntityRowWithFeatures) {
-	entityRows := []types.EntityRow{
-		{
-			EntityKey: "1234",
-			UnixTime:  10,
-		},
-		{
-			EntityKey: "1234",
-			UnixTime:  20,
-		},
-		{
-			EntityKey: "1235",
-			UnixTime:  15,
-		},
-		{
-			EntityKey: "1236",
-			UnixTime:  20,
-		},
-	}
-	joinResult := map[string]map[string]dbutil.RowMap{
-		"device_basic": {
-			"1234,10": {
+func prepareResults() (<-chan dbutil.RowMapRecord, []*types.EntityRowWithFeatures) {
+	joined := make(chan dbutil.RowMapRecord)
+	go func() {
+		defer close(joined)
+		joined <- dbutil.RowMapRecord{
+			RowMap: dbutil.RowMap{
 				"model":      "apple",
-				"entity_key": "1234",
-				"unix_time":  10,
-			},
-			"1234,20": {
-				"model":      "oneplus",
-				"entity_key": "1234",
-				"unix_time":  20,
-			},
-			"1235,15": {
-				"model":      "huawei",
-				"entity_key": "1235",
-				"unix_time":  15,
-			},
-		},
-		"device_advanced": {
-			"1234,10": {
 				"price":      100,
 				"entity_key": "1234",
 				"unix_time":  10,
 			},
-			"1234,20": {
+		}
+		joined <- dbutil.RowMapRecord{
+			RowMap: dbutil.RowMap{
+				"model":      "oneplus",
 				"price":      120,
 				"entity_key": "1234",
 				"unix_time":  20,
 			},
-			"1235,15": {
+		}
+		joined <- dbutil.RowMapRecord{
+			RowMap: dbutil.RowMap{
+				"model":      "huawei",
 				"price":      90,
 				"entity_key": "1235",
 				"unix_time":  15,
 			},
-		},
-	}
+		}
+	}()
+
 	result := []*types.EntityRowWithFeatures{
 		{
 			EntityRow: types.EntityRow{
@@ -218,20 +212,7 @@ func prepareEntityRowsAndResult() ([]types.EntityRow, map[string]map[string]dbut
 				},
 			},
 		},
-		{
-			EntityRow: types.EntityRow{
-				EntityKey: "1236",
-				UnixTime:  20,
-			},
-			FeatureValues: []types.FeatureKV{
-				{
-					FeatureName: "model",
-				},
-				{
-					FeatureName: "price",
-				},
-			},
-		},
 	}
-	return entityRows, joinResult, result
+
+	return joined, result
 }
