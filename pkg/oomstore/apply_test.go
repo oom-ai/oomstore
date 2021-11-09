a415dae05b8f5fa86a2bb4e88a82d94dcbdf71c3
Yik San Chan <evan.chanyiksan@gmail.com>
2021-11-09 20:37:04 +0800

    feat(oomstore): app apply processing framework

---
+++ pkg/oomstore/apply_test.go
@@ -0,0 +1,43 @@
+package oomstore_test
+
+import (
+	"context"
+	"strings"
+	"testing"
+
+	"github.com/oom-ai/oomstore/pkg/oomstore"
+	"github.com/oom-ai/oomstore/pkg/oomstore/types/apply"
+	"github.com/stretchr/testify/require"
+)
+
+func TestApplyEntity(t *testing.T) {
+	var entityYamlData = `
+kind: Entity
+name: user
+length: 8
+description: a description
+batch-features:
+- group: device
+  description: a description
+  features:
+  - name: model
+    db-type-value: varchar(16)
+  - name: price
+    db_type_value: int
+-  group: user
+   description: a description
+   features:
+   - name: age
+     db-type-value: int
+   - name: gender
+     db-type-value: int
+stream-features:
+- name: c
+  db-type-value: xxx
+`
+	store := oomstore.NewOomStore(nil, nil, nil)
+
+	require.Nil(t, store.Apply(context.Background(), apply.ApplyOpt{
+		R: strings.NewReader(entityYamlData),
+	}))
+}
