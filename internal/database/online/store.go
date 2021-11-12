648552b346150e3228348e2ae52203a8a272bd4f
Yik San Chan <evan.chanyiksan@gmail.com>
2021-11-12 16:26:52 +0800

    refactor: Purge by revisionID

--- internal/database/online/store.go
+++ internal/database/online/store.go
@@ -5,13 +5,12 @@ import (
 	"io"
 
 	"github.com/oom-ai/oomstore/internal/database/dbutil"
-	"github.com/oom-ai/oomstore/pkg/oomstore/typesv2"
 )
 
 type Store interface {
 	Get(ctx context.Context, opt GetOpt) (dbutil.RowMap, error)
 	MultiGet(ctx context.Context, opt MultiGetOpt) (map[string]dbutil.RowMap, error)
 	Import(ctx context.Context, opt ImportOpt) error
-	Purge(ctx context.Context, revision *typesv2.Revision) error
+	Purge(ctx context.Context, revisionID int32) error
 	io.Closer
 }
