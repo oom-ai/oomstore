cc2dda1126ec898bd4183500e2016572332a0fd1
lianxmfor <1034552569@qq.com>
2021-11-12 14:57:36 +0800

    test: fix test update feature

--- featctl/test/test_update_feature.sh
+++ featctl/test/test_update_feature.sh
@@ -4,24 +4,19 @@ source ./util.sh
 
 init_store
 register_features
-import_sample >> /dev/null
+import_sample > /dev/null
 
 case='featctl update feature works'
 featctl update feature price --description "new description"
 expected='
-Name:                     price
-Group:                    phone
-Entity:                   device
-Category:                 batch
-DBValueType:              int
-ValueType:                int32
-Description:              new description
-Online Revision:          <NULL>
-Offline Latest Revision:  <NULL>
-Offline Latest DataTable: <NULL>
-CreateTime:
-ModifyTime:
+Name:            price
+Group:           phone
+Entity:          device
+Category:        batch
+DBValueType:     int
+ValueType:       int32
+Description:     new description
 '
 actual=$(featctl describe feature price)
 ignore() { grep -Ev '^(CreateTime|ModifyTime|Online Revision|Offline Latest Revision|Offline Latest DataTable)' <<<"$1"; }
-assert_eq "$case" "$(ignore "$expected")" "$(ignore "$actual")"
+assert_eq "$case"  "$expected" "$(ignore "$actual")"
