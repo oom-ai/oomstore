127fc97605974c0031d67df632caf972dde94796
lianxmfor <1034552569@qq.com>
2021-11-12 11:19:03 +0800

    test(metastore/test): add helper for test

---
+++ internal/database/metadatav2/test/helper.go
@@ -0,0 +1,17 @@
+package test
+
+func boolPtr(i bool) *bool {
+	return &i
+}
+
+func int16Ptr(i int16) *int16 {
+	return &i
+}
+
+func int32Ptr(i int32) *int32 {
+	return &i
+}
+
+func stringPtr(s string) *string {
+	return &s
+}
