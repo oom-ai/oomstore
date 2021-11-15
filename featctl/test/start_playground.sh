93c56c5e9326aabd5aa0b704cf67093522473d6b
Yik San Chan <evan.chanyiksan@gmail.com>
2021-11-15 16:15:39 +0800

    fix: rename test db to resolve conflicts with quickstart

--- featctl/test/start_playground.sh
+++ featctl/test/start_playground.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-docker run --rm -d --name oomstore \
+docker run --rm -d --name oomstore_test \
     -e POSTGRES_PASSWORD=postgres \
     -e POSTGRES_USER=postgres \
     -p 5432:5432 \
     postgres:14.0-alpine >/dev/null
 
-docker logs -f oomstore
+docker logs -f oomstore_test
