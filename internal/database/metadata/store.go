383df675fdca7427a03e9a3f97932d745a1b78bf
Wenxuan Zhang <wenxuangm@gmail.com>
2021-11-16 13:20:30 +0800

    feat: add write api with transaction for metadata

--- internal/database/metadata/store.go
+++ internal/database/metadata/store.go
@@ -8,35 +8,45 @@ import (
 )
 
 type Store interface {
-	// entity
-	CreateEntity(ctx context.Context, opt CreateEntityOpt) (int16, error)
-	UpdateEntity(ctx context.Context, opt UpdateEntityOpt) error
+	io.Closer
+	StoreRead
+	StoreWrite
+}
+
+type StoreRead interface {
 	GetEntity(ctx context.Context, id int16) (*types.Entity, error)
 	GetEntityByName(ctx context.Context, name string) (*types.Entity, error)
 	ListEntity(ctx context.Context) types.EntityList
 
-	// feature
-	CreateFeature(ctx context.Context, opt CreateFeatureOpt) (int16, error)
-	UpdateFeature(ctx context.Context, opt UpdateFeatureOpt) error
 	GetFeature(ctx context.Context, id int16) (*types.Feature, error)
 	GetFeatureByName(ctx context.Context, name string) (*types.Feature, error)
 	ListFeature(ctx context.Context, opt ListFeatureOpt) types.FeatureList
 
-	// feature group
-	CreateFeatureGroup(ctx context.Context, opt CreateFeatureGroupOpt) (int16, error)
-	UpdateFeatureGroup(ctx context.Context, opt UpdateFeatureGroupOpt) error
 	GetFeatureGroup(ctx context.Context, id int16) (*types.FeatureGroup, error)
 	GetFeatureGroupByName(ctx context.Context, name string) (*types.FeatureGroup, error)
 	ListFeatureGroup(ctx context.Context, entityID *int16) types.FeatureGroupList
 
-	// revision
-	CreateRevision(ctx context.Context, opt CreateRevisionOpt) (int32, string, error)
-	UpdateRevision(ctx context.Context, opt UpdateRevisionOpt) error
 	GetRevision(ctx context.Context, id int32) (*types.Revision, error)
 	GetRevisionBy(ctx context.Context, groupID int16, revision int64) (*types.Revision, error)
 	ListRevision(ctx context.Context, opt ListRevisionOpt) types.RevisionList
 
-	// refresh
 	Refresh() error
-	io.Closer
+}
+
+type StoreWrite interface {
+	// entity
+	CreateEntity(ctx context.Context, opt CreateEntityOpt) (int16, error)
+	UpdateEntity(ctx context.Context, opt UpdateEntityOpt) error
+
+	// feature
+	CreateFeature(ctx context.Context, opt CreateFeatureOpt) (int16, error)
+	UpdateFeature(ctx context.Context, opt UpdateFeatureOpt) error
+
+	// feature group
+	CreateFeatureGroup(ctx context.Context, opt CreateFeatureGroupOpt) (int16, error)
+	UpdateFeatureGroup(ctx context.Context, opt UpdateFeatureGroupOpt) error
+
+	// revision
+	CreateRevision(ctx context.Context, opt CreateRevisionOpt) (int32, string, error)
+	UpdateRevision(ctx context.Context, opt UpdateRevisionOpt) error
 }
