6579a0392ac48947faeb0fcf6893ac683386e0d9
lianxmfor <1034552569@qq.com>
2021-11-23 19:35:02 +0800

    test(oomd/test): make each method test in separate methods

--- oomd/test/client.go
+++ oomd/test/client.go
@@ -13,7 +13,8 @@ import (
 )
 
 var (
-	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+	addr     = flag.String("addr", "localhost:50051", "the address to connect to")
+	function = flag.String("func", "", "the function to be tested")
 )
 
 // The method temporarily serves the testing purpose while we iterate fast
@@ -21,13 +22,35 @@ var (
 // We will delete this file anyway once we have more formal tests
 func main() {
 	flag.Parse()
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(*addr, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+
+	switch *function {
+	case "online-get":
+		OnlineGet()
+	case "import":
+		Import()
+	default:
+		log.Fatalf("invalid function: %s", *function)
 	}
-	defer conn.Close()
-	c := codegen.NewOomDClient(conn)
+}
+
+func OnlineGet() {
+	c, cancel := prepareOomDClient(*addr)
+	defer cancel()
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.OnlineGet(ctx, &codegen.OnlineGetRequest{EntityKey: "1", FeatureNames: []string{"model"}})
+	if err != nil {
+		log.Fatalf("could not get: %v", err)
+	}
+	log.Printf("Got: %v", r)
+}
+
+func Import() {
+	c, cancel := prepareOomDClient(*addr)
+	defer cancel()
 
 	// Contact the server and print out its response.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
@@ -57,10 +80,12 @@ func main() {
 		panic(err)
 	}
 	log.Printf("Import Got: %v", importRes)
+}
 
-	r, err := c.OnlineGet(ctx, &codegen.OnlineGetRequest{EntityKey: "1", FeatureNames: []string{"model"}})
+func prepareOomDClient(addr string) (c codegen.OomDClient, cancel func()) {
+	conn, err := grpc.Dial(addr, grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("could not get: %v", err)
+		log.Fatalf("did not connect: %v", err)
 	}
-	log.Printf("Got: %v", r)
+	return codegen.NewOomDClient(conn), func() { conn.Close() }
 }
