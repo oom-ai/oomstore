0b4cb65ff956eeb6cde71f3332276be9a4c8c9e5
Yik San Chan <evan.chanyiksan@gmail.com>
2021-11-23 19:33:10 +0800

    feat: python call grpc

---
+++ oomd/client.py
@@ -0,0 +1,12 @@
+import grpc
+import codegen.oomd_pb2
+import codegen.oomd_pb2_grpc
+
+def online_get(entity_key, feature_names):
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = codegen.oomd_pb2_grpc.OomDStub(channel)
+        response = stub.OnlineGet(codegen.oomd_pb2.OnlineGetRequest(entity_key=entity_key, feature_names=feature_names))
+    return response.result.map
+
+if __name__ == "__main__":
+    print(online_get("1006", ["state", "credit_score", "account_age_days", "has_2fa_installed", "transaction_count_7d", "transaction_count_30d"]))
