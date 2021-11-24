363c45d73bc25ef7cc5d060d12a5187443f96749
lianxmfor <1034552569@qq.com>
2021-11-24 19:26:07 +0800

    feat(sdk/python): start oomd asynchronously at initialisation

--- sdk/python/client.py
+++ sdk/python/client.py
@@ -1,51 +1,145 @@
+import sys
 import grpc
+import logging
 import codegen.oomd_pb2
 import codegen.oomd_pb2_grpc
+import time
+from pathlib import Path
+from subprocess import Popen
 
 # Convert google.protobuf.pyext._message.MessageMapContainer object to Python dictionary
 def map_container_to_dict(map_container):
-    return dict({k: getattr(v, v.WhichOneof('kind')) for k, v in map_container.items()})
+    return dict({k: getattr(v, v.WhichOneof("kind")) for k, v in map_container.items()})
+
 
 class Client(object):
-    def __init__(self):
-        pass
+    def __init__(self, port: int, config_path: str):
+        try:
+            self.oomd = Popen(["oomd", "--config", config_path])
+        except Exception as e:
+            logging.error(e)
+            sys.exit(1)
+
+        self.addr = "localhost:%d" % port
+
+        # wait for oomed started
+        time.sleep(2)
+
+    def __del__(self):
+        self.oomd.terminate()
 
     def online_get(self, entity_key, feature_names):
-        with grpc.insecure_channel('localhost:50051') as channel:
+        with grpc.insecure_channel(self.addr) as channel:
             stub = codegen.oomd_pb2_grpc.OomDStub(channel)
-            response = stub.OnlineGet(codegen.oomd_pb2.OnlineGetRequest(entity_key=entity_key, feature_names=feature_names))
+            response = stub.OnlineGet(
+                codegen.oomd_pb2.OnlineGetRequest(
+                    entity_key=entity_key, feature_names=feature_names
+                )
+            )
         return map_container_to_dict(response.result.map)
 
     def online_multi_get(self, entity_keys, feature_names):
-        with grpc.insecure_channel('localhost:50051') as channel:
+        with grpc.insecure_channel(self.addr) as channel:
             stub = codegen.oomd_pb2_grpc.OomDStub(channel)
-            response = stub.OnlineMultiGet(codegen.oomd_pb2.OnlineMultiGetRequest(entity_keys=entity_keys, feature_names=feature_names))
-        return dict({entity_key: map_container_to_dict(values.map) for entity_key, values in response.result.items()})
+            response = stub.OnlineMultiGet(
+                codegen.oomd_pb2.OnlineMultiGetRequest(
+                    entity_keys=entity_keys, feature_names=feature_names
+                )
+            )
+        return dict(
+            {
+                entity_key: map_container_to_dict(values.map)
+                for entity_key, values in response.result.items()
+            }
+        )
 
     def sync(self, revision_id):
-        with grpc.insecure_channel('localhost:50051') as channel:
+        with grpc.insecure_channel(self.addr) as channel:
             stub = codegen.oomd_pb2_grpc.OomDStub(channel)
             stub.Sync(codegen.oomd_pb2.SyncRequest(revision_id=revision_id))
         return
 
-    def import_by_file(self, group_name, description, input_file_path, delimiter, revision=None):
-        with grpc.insecure_channel('localhost:50051') as channel:
+    def import_by_file(
+        self, group_name, description, input_file_path, delimiter, revision=None
+    ):
+        with grpc.insecure_channel(self.addr) as channel:
             stub = codegen.oomd_pb2_grpc.OomDStub(channel)
-            response = stub.ImportByFile(codegen.oomd_pb2.ImportByFileRequest(group_name=group_name,description=description,input_file_path=input_file_path,delimiter=delimiter,revision=revision))
+            response = stub.ImportByFile(
+                codegen.oomd_pb2.ImportByFileRequest(
+                    group_name=group_name,
+                    description=description,
+                    input_file_path=input_file_path,
+                    delimiter=delimiter,
+                    revision=revision,
+                )
+            )
         return response.revision_id
 
     def join_by_file(self, feature_names, input_file_path, output_file_path):
-        with grpc.insecure_channel('localhost:50051') as channel:
+        with grpc.insecure_channel(self.addr) as channel:
             stub = codegen.oomd_pb2_grpc.OomDStub(channel)
-            stub.JoinByFile(codegen.oomd_pb2.JoinByFileRequest(feature_names=feature_names,input_file_path=input_file_path,output_file_path=output_file_path))
+            stub.JoinByFile(
+                codegen.oomd_pb2.JoinByFileRequest(
+                    feature_names=feature_names,
+                    input_file_path=input_file_path,
+                    output_file_path=output_file_path,
+                )
+            )
         return
 
+
 if __name__ == "__main__":
-    client = Client()
-    revision_id1 = client.import_by_file(group_name='account', description='sample account data', input_file_path='/tmp/account.csv', delimiter=',')
-    revision_id2 = client.import_by_file(group_name='transaction_stats', description='sample transaction stat data', input_file_path='/tmp/transaction_stats.csv', delimiter=',')
+    config_path = "%s/.config/oomstore/config.yaml" % str(Path.home())
+    client = Client(50051, config_path)
+    revision_id1 = client.import_by_file(
+        group_name="account",
+        description="sample account data",
+        input_file_path="/tmp/account.csv",
+        delimiter=",",
+    )
+    revision_id2 = client.import_by_file(
+        group_name="transaction_stats",
+        description="sample transaction stat data",
+        input_file_path="/tmp/transaction_stats.csv",
+        delimiter=",",
+    )
     client.sync(revision_id1)
     client.sync(revision_id2)
-    print(client.online_get(entity_key="1006", feature_names=["state", "credit_score", "account_age_days", "has_2fa_installed", "transaction_count_7d", "transaction_count_30d"]))
-    print(client.online_multi_get(entity_keys=["1006", "1007"], feature_names=["state", "credit_score", "account_age_days", "has_2fa_installed", "transaction_count_7d", "transaction_count_30d"]))
-    client.join_by_file(feature_names=["state", "credit_score", "account_age_days", "has_2fa_installed", "transaction_count_7d", "transaction_count_30d"], input_file_path='/tmp/label.csv', output_file_path='/tmp/joined.csv')
+    print(
+        client.online_get(
+            entity_key="1006",
+            feature_names=[
+                "state",
+                "credit_score",
+                "account_age_days",
+                "has_2fa_installed",
+                "transaction_count_7d",
+                "transaction_count_30d",
+            ],
+        )
+    )
+    print(
+        client.online_multi_get(
+            entity_keys=["1006", "1007"],
+            feature_names=[
+                "state",
+                "credit_score",
+                "account_age_days",
+                "has_2fa_installed",
+                "transaction_count_7d",
+                "transaction_count_30d",
+            ],
+        )
+    )
+    client.join_by_file(
+        feature_names=[
+            "state",
+            "credit_score",
+            "account_age_days",
+            "has_2fa_installed",
+            "transaction_count_7d",
+            "transaction_count_30d",
+        ],
+        input_file_path="/tmp/label.csv",
+        output_file_path="/tmp/joined.csv",
+    )
