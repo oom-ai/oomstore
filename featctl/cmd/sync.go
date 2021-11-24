8465349a6611d35c20839b4233a3d87ced30a138
Wenxuan Zhang <wenxuangm@gmail.com>
2021-11-24 19:01:01 +0800

    feat: support delay purge

--- featctl/cmd/sync.go
+++ featctl/cmd/sync.go
@@ -33,4 +33,6 @@ func init() {
 
 	flags.IntVarP(&syncOpt.RevisionID, "revision-id", "r", 0, "group revision id")
 	_ = syncCmd.MarkFlagRequired("revision-id")
+
+	flags.IntVarP(&syncOpt.PurgeDelay, "purge-delay", "", 0, "wait time in seconds before purging the old revision")
 }
