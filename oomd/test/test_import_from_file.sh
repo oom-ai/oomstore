dd9cbac1b445795d82234711477a5464be622a01
Wenxuan Zhang <wenxuangm@gmail.com>
2021-11-25 21:00:35 +0800

    test: add grpc import-from-file test

---
+++ oomd/test/test_import_from_file.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+SDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SDIR" || exit 1
+source ./util.sh
+
+prepare_store
+prepare_oomd
+
+case="api returns ok"
+arg=$(cat <<-EOF
+{
+    "group_name": "account",
+    "input_file_path": "./data/account_10.csv",
+    "delimiter": ","
+}
+EOF
+)
+expected='
+{
+  "status": {},
+  "revisionId": "3"
+}
+'
+actual=$(testgrpc Import <<<"$arg")
+assert_json_eq "$case" "$expected" "$actual"
+
+case="data actually imported"
+expected='
+user,state,credit_score,account_age_days,has_2fa_installed
+10,Idaho,693,212,true
+'
+oomctl sync -r 3
+actual=$(oomctl get online --feature state,credit_score,account_age_days,has_2fa_installed -k 10 -o csv)
+assert_eq "$case" "$expected" "$actual"
