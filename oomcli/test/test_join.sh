f8631b67fd5f0b06636aebbe3ca5aaaf381a393e
Jinghan Ying <jinghancc@gmail.com>
2021-11-29 15:53:29 +0800

    test(oomcli/test): modify integration test

--- oomcli/test/test_join.sh
+++ oomcli/test/test_join.sh
@@ -6,18 +6,21 @@ init_store
 register_features
 
 # clean up the tmp file
-trap 'command rm -rf entity_rows.csv' EXIT INT TERM HUP
+trap 'command rm -rf entity_rows.csv entity_rows_with_values.csv' EXIT INT TERM HUP
+
+# import sample data to offline store
+import_sample 80
 
 t1=50
 t2=100
 cat <<-EOF > entity_rows.csv
+entity_key,unix_milli
 1,$t1
 2,$t1
 1,$t2
 2,$t2
 EOF
 
-import_sample 80
 case='oomcli join historical-feature'
 expected="
 entity_key,unix_milli,model,price
@@ -26,10 +29,38 @@ entity_key,unix_milli,model,price
 1,$t2,xiaomi-mix3,3999
 2,$t2,huawei-p40,5299
 "
-
 actual=$(oomcli join \
     --feature model,price \
     --input-file entity_rows.csv \
+    --output csv
+)
+
+sorted_expected=$(echo "$expected"|sort)
+sorted_actual=$(echo "$actual"|sort)
+
+assert_eq "$case" "$sorted_expected" "$sorted_actual"
+
+
+cat <<-EOF > entity_rows_with_values.csv
+entity_key,unix_milli,value_1,value_2
+1,$t1,1,2
+2,$t1,3,4
+1,$t2,5,6
+2,$t2,7,8
+EOF
+
+case='oomcli join historical-feature with real-time feature values'
+expected="
+entity_key,unix_milli,value_1,value_2,model,price
+1,$t1,1,2,,
+2,$t1,3,4,,
+1,$t2,5,6,xiaomi-mix3,3999
+2,$t2,7,8,huawei-p40,5299
+"
+
+actual=$(oomcli join \
+    --feature model,price \
+    --input-file entity_rows_with_values.csv \
     --output csv
 )
 
