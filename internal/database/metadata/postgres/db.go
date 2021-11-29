d758e451b490ff9a015645496cb5a75b9e641744
Jinghan Ying <jinghancc@gmail.com>
2021-11-29 20:34:23 +0800

    feat(database/metadata): impl GetGroup and GetGroupByName

--- internal/database/metadata/postgres/db.go
+++ internal/database/metadata/postgres/db.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/oom-ai/oomstore/internal/database/metadata"
+	"github.com/oom-ai/oomstore/pkg/oomstore/types"
 )
 
 func (db *DB) CreateEntity(ctx context.Context, opt metadata.CreateEntityOpt) (int, error) {
@@ -20,6 +21,14 @@ func (db *DB) CreateGroup(ctx context.Context, opt metadata.CreateGroupOpt) (int
 
 func (db *DB) UpdateGroup(ctx context.Context, opt metadata.UpdateGroupOpt) error {
 	return updateGroup(ctx, db, opt)
+}
+
+func (db *DB) GetGroup(ctx context.Context, id int) (*types.Group, error) {
+	return getGroup(ctx, db, id)
+}
+
+func (db *DB) GetGroupByName(ctx context.Context, name string) (*types.Group, error) {
+	return getGroupByName(ctx, db, name)
 }
 
 func (db *DB) CreateFeature(ctx context.Context, opt metadata.CreateFeatureOpt) (int, error) {
