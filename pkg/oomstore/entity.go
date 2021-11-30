6e9f3019ac831c414c3bf876ef9609564d868a48
Jinghan Ying <jinghancc@gmail.com>
2021-11-30 14:43:34 +0800

    refactor: switch to db-based methods for Get/ListEntity

--- pkg/oomstore/entity.go
+++ pkg/oomstore/entity.go
@@ -2,7 +2,6 @@ package oomstore
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/oom-ai/oomstore/internal/database/metadata"
 	"github.com/oom-ai/oomstore/pkg/oomstore/types"
@@ -10,31 +9,21 @@ import (
 
 // Get metadata of an entity by ID.
 func (s *OomStore) GetEntity(ctx context.Context, id int) (*types.Entity, error) {
-	if err := s.metadata.Refresh(); err != nil {
-		return nil, fmt.Errorf("failed to refresh informer, err=%+v", err)
-	}
-	return s.metadata.CacheGetEntity(ctx, id)
+	return s.metadata.GetEntity(ctx, id)
 }
 
 // Get metadata of an entity by name.
 func (s *OomStore) GetEntityByName(ctx context.Context, name string) (*types.Entity, error) {
-	if err := s.metadata.Refresh(); err != nil {
-		return nil, fmt.Errorf("failed to refresh informer, err=%+v", err)
-	}
-	return s.metadata.CacheGetEntityByName(ctx, name)
+	return s.metadata.GetEntityByName(ctx, name)
 }
 
 // List metadata of all entities.
-func (s *OomStore) ListEntity(ctx context.Context) types.EntityList {
-	_ = s.metadata.Refresh()
-	return s.metadata.CacheListEntity(ctx)
+func (s *OomStore) ListEntity(ctx context.Context) (types.EntityList, error) {
+	return s.metadata.ListEntity(ctx, nil)
 }
 
 // Create metadata for an entity.
 func (s *OomStore) CreateEntity(ctx context.Context, opt types.CreateEntityOpt) (int, error) {
-	if err := s.metadata.Refresh(); err != nil {
-		return 0, fmt.Errorf("failed to refresh informer, err=%+v", err)
-	}
 	return s.metadata.CreateEntity(ctx, metadata.CreateEntityOpt{
 		CreateEntityOpt: opt,
 	})
@@ -42,10 +31,7 @@ func (s *OomStore) CreateEntity(ctx context.Context, opt types.CreateEntityOpt) 
 
 // Update metadata for an entity.
 func (s *OomStore) UpdateEntity(ctx context.Context, opt types.UpdateEntityOpt) error {
-	if err := s.metadata.Refresh(); err != nil {
-		return fmt.Errorf("failed to refresh informer, err=%+v", err)
-	}
-	entity, err := s.metadata.CacheGetEntityByName(ctx, opt.EntityName)
+	entity, err := s.metadata.GetEntityByName(ctx, opt.EntityName)
 	if err != nil {
 		return err
 	}
