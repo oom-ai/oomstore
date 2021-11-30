507039328d088cb3e499d7f533b04bbe06cc712c
lianxmfor <1034552569@qq.com>
2021-11-30 11:42:09 +0800

    feat(database/metadata): implement db-based method GetFeature

--- internal/database/metadata/store.go
+++ internal/database/metadata/store.go
@@ -23,6 +23,7 @@ type ReadStore interface {
 	CacheListEntity(ctx context.Context) types.EntityList
 	ListEntity(ctx context.Context) (types.EntityList, error)
 
+	GetFeature(ctx context.Context, id int) (*types.Feature, error)
 	CacheGetFeature(ctx context.Context, id int) (*types.Feature, error)
 	CacheGetFeatureByName(ctx context.Context, name string) (*types.Feature, error)
 	CacheListFeature(ctx context.Context, opt ListFeatureOpt) types.FeatureList
