6e80d33f7953a93fed9c7a34fd832d3dda4504db
Jinghan Ying <jinghancc@gmail.com>
2021-12-01 15:36:35 +0800

    refactor(oomcli): rename command list feature to get meta feature

--- oomcli/test/test_get_meta_feature.sh
+++ oomcli/test/test_get_meta_feature.sh
@@ -6,11 +6,11 @@ init_store
 register_features
 import_sample > /dev/null
 
-case='oomcli list feature works'
+case='oomcli get meta feature works'
 expected='Name,Group,Entity,Category,DBValueType,ValueType,Description,OnlineRevisionID
 model,phone,device,batch,varchar(32),string,model,<NULL>
 price,phone,device,batch,int,int64,price,<NULL>
 '
-actual=$(oomcli list feature -o csv)
+actual=$(oomcli get meta feature -o csv)
 ignore_time() { cut -d ',' -f 1-8 <<<"$1"; }
 assert_eq "$case" "$(sort <<< "$expected")" "$(ignore_time "$actual" | sort)"
