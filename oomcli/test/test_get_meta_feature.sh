947c55dae07ed559a8432f24ec6df5dc357b96df
lianxmfor <1034552569@qq.com>
2021-12-02 19:38:14 +0800

    test(oomcli): modity integration-test for test get meta ouput yaml

--- oomcli/test/test_get_meta_feature.sh
+++ oomcli/test/test_get_meta_feature.sh
@@ -10,6 +10,7 @@ case='oomcli get meta features works'
 expected='ID,NAME,GROUP,ENTITY,CATEGORY,DB-VALUE-TYPE,VALUE-TYPE,DESCRIPTION,ONLINE-REVISION-ID
 1,price,phone,device,batch,int,int64,price,<NULL>
 2,model,phone,device,batch,varchar(32),string,model,<NULL>
+3,age,student,user,batch,int,int64,age,<NULL>
 '
 actual=$(oomcli get meta feature -o csv --wide)
 ignore_time() { cut -d ',' -f 1-9 <<<"$1"; }
@@ -19,6 +20,7 @@ case='oomcli get simplified meta features works'
 expected='ID,NAME,GROUP,ENTITY,CATEGORY,VALUE-TYPE,DESCRIPTION
 1,price,phone,device,batch,int64,price
 2,model,phone,device,batch,string,model
+3,age,student,user,batch,int64,age
 '
 actual=$(oomcli get meta feature -o csv)
 assert_eq "$case" "$(sort <<< "$expected")" "$(sort <<< "$actual")"
@@ -38,7 +40,7 @@ kind: Feature
 name: model
 group-name: phone
 db-value-type: varchar(32)
-description: varchar(32)
+description: model
 '
 
 actual=$(oomcli get meta feature model -o yaml)
@@ -51,12 +53,17 @@ items:
       name: price
       group-name: phone
       db-value-type: int
-      description: int
+      description: price
     - kind: Feature
       name: model
       group-name: phone
       db-value-type: varchar(32)
-      description: varchar(32)
+      description: model
+    - kind: Feature
+      name: age
+      group-name: student
+      db-value-type: int
+      description: age
 '
 
 actual=$(oomcli get meta feature -o yaml)
