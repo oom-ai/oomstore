233fe1660ba7926e88537ca7e8021d5c1ce36527
Jinghan Ying <jinghancc@gmail.com>
2021-12-02 19:26:50 +0800

    feat(database/metadata): use ListMetaData for mysql and postgres

--- internal/database/metadata/mysql/database.go
+++ internal/database/metadata/mysql/database.go
@@ -8,6 +8,7 @@ import (
 	"github.com/jmoiron/sqlx"
 	"github.com/oom-ai/oomstore/internal/database/dbutil"
 	"github.com/oom-ai/oomstore/internal/database/metadata/informer"
+	"github.com/oom-ai/oomstore/internal/database/metadata/sqlutil"
 	"github.com/oom-ai/oomstore/pkg/oomstore/types"
 )
 
@@ -45,7 +46,7 @@ func Open(ctx context.Context, option *types.MySQLOpt) (*DB, error) {
 
 	// TODO: make the interval configurable
 	informer, err := informer.New(time.Second, func() (*informer.Cache, error) {
-		return list(ctx, db)
+		return sqlutil.ListMetaData(ctx, db)
 	})
 	if err != nil {
 		db.Close()
@@ -109,28 +110,4 @@ func createMetaSchemas(ctx context.Context, db *sqlx.DB) (err error) {
 
 		return nil
 	})
-}
-
-func list(ctx context.Context, db *sqlx.DB) (*informer.Cache, error) {
-	var cache *informer.Cache
-	err := dbutil.WithTransaction(db, ctx, func(ctx context.Context, tx *sqlx.Tx) error {
-		entities := types.EntityList{}
-		if err := tx.SelectContext(ctx, &entities, `SELECT * FROM entity`); err != nil {
-			return err
-		}
-
-		features := types.FeatureList{}
-		if err := tx.SelectContext(ctx, &features, `SELECT * FROM feature`); err != nil {
-			return err
-		}
-
-		groups := types.GroupList{}
-		if err := tx.SelectContext(ctx, &groups, `SELECT * FROM feature_group`); err != nil {
-			return err
-		}
-
-		cache = informer.NewCache(entities, features, groups)
-		return nil
-	})
-	return cache, err
 }
