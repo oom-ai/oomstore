f9fe1cd42c41968d164e2ec5e9819a15083a1959
Jinghan Ying <jinghancc@gmail.com>
2021-12-02 20:08:05 +0800

    test(database/metadata): add unit test for entity

---
+++ internal/database/metadata/mysql/db_test.go
@@ -0,0 +1,23 @@
+package mysql_test
+
+import (
+	"testing"
+
+	"github.com/oom-ai/oomstore/internal/database/metadata/test_impl"
+)
+
+func TestCreateEntity(t *testing.T) {
+	test_impl.TestCreateEntity(t, prepareStore)
+}
+
+func TestGetEntity(t *testing.T) {
+	test_impl.TestGetEntity(t, prepareStore)
+}
+
+func TestUpdateEntity(t *testing.T) {
+	test_impl.TestUpdateEntity(t, prepareStore)
+}
+
+func TestListEntity(t *testing.T) {
+	test_impl.TestListEntity(t, prepareStore)
+}
