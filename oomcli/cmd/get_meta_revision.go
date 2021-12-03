2e307223c720de9e900f95abdc1aabeef82547ce
Wenxuan Zhang <wenxuangm@gmail.com>
2021-12-03 16:31:46 +0800

    feat: implement oomcli get meta revision

---
+++ oomcli/cmd/get_meta_revision.go
@@ -0,0 +1,63 @@
+package cmd
+
+import (
+	"context"
+	"log"
+	"strconv"
+
+	"github.com/oom-ai/oomstore/pkg/oomstore/types"
+	"github.com/spf13/cobra"
+)
+
+type getRevisionOption struct {
+	groupName *string
+}
+
+var getRevisionOpt getRevisionOption
+
+var getMetaRevisionCmd = &cobra.Command{
+	Use:   "revision",
+	Short: "get existing revision given specific conditions",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) > 1 {
+			log.Fatalf("argument at most one, got %d", len(args))
+		}
+
+		ctx := context.Background()
+		oomStore := mustOpenOomStore(ctx, oomStoreCfg)
+		defer oomStore.Close()
+
+		var groupID *int
+
+		if getRevisionOpt.groupName != nil {
+			group, err := oomStore.GetGroupByName(ctx, *getRevisionOpt.groupName)
+			if err != nil {
+				log.Fatalf("failed to get feature group name=%s: %v", *getRevisionOpt.groupName, err)
+			}
+			groupID = &group.ID
+		}
+
+		revisions, err := oomStore.ListRevision(ctx, groupID)
+		if err != nil {
+			log.Fatalf("failed geting revisions, error %v\n", err)
+		}
+
+		if len(args) > 0 {
+			revisions = revisions.Filter(func(r *types.Revision) bool {
+				return strconv.Itoa(r.ID) == args[0]
+			})
+		}
+
+		// print revisions to stdout
+		if err := serializeMetadata(revisions, *getMetaOutput, *getMetaWide); err != nil {
+			log.Fatalf("failed printing entities, error %v\n", err)
+		}
+	},
+}
+
+func init() {
+	getMetaCmd.AddCommand(getMetaRevisionCmd)
+
+	flags := getMetaRevisionCmd.Flags()
+	getRevisionOpt.groupName = flags.StringP("group", "g", "", "feature group")
+}
