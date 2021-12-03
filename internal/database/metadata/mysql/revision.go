2bc574d35b6dfa031198773c586bba386afd55a9
Jinghan Ying <jinghancc@gmail.com>
2021-12-03 14:08:58 +0800

    feat(database/metadata): implement createRevision for mysql

---
+++ internal/database/metadata/mysql/revision.go
@@ -0,0 +1,48 @@
+package mysql
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/oom-ai/oomstore/internal/database/metadata"
+)
+
+func createRevision(ctx context.Context, sqlxCtx metadata.SqlxContext, opt metadata.CreateRevisionOpt) (int, string, error) {
+	var dataTable string
+	if opt.DataTable != nil {
+		dataTable = *opt.DataTable
+	}
+
+	insertQuery := "INSERT INTO feature_group_revision(group_id, revision, data_table, anchored, description) VALUES (?, ?, ?, ?, ?)"
+	res, err := sqlxCtx.ExecContext(ctx, sqlxCtx.Rebind(insertQuery), opt.GroupID, opt.Revision, dataTable, opt.Anchored, opt.Description)
+	if err != nil {
+		if er, ok := err.(*mysql.MySQLError); ok {
+			if er.Number == ER_DUP_ENTRY {
+				return 0, "", fmt.Errorf("revision already exists: groupID=%d, revision=%d", opt.GroupID, opt.Revision)
+			}
+		}
+		return 0, "", err
+	}
+	revisionID, err := res.LastInsertId()
+	if err != nil {
+		return 0, "", err
+	}
+
+	if opt.DataTable == nil {
+		updateQuery := "UPDATE feature_group_revision SET data_table = ? WHERE id = ?"
+		dataTable = fmt.Sprintf("offline_%d_%d", opt.GroupID, revisionID)
+		result, err := sqlxCtx.ExecContext(ctx, sqlxCtx.Rebind(updateQuery), dataTable, revisionID)
+		if err != nil {
+			return 0, "", err
+		}
+		rowsAffected, err := result.RowsAffected()
+		if err != nil {
+			return 0, "", err
+		}
+		if rowsAffected != 1 {
+			return 0, "", fmt.Errorf("failed to update revision %d: revision not found", revisionID)
+		}
+	}
+	return int(revisionID), dataTable, nil
+}
