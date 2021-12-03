eebb20db47047e856e65c2a396a3cc692a18db79
Wenxuan Zhang <wenxuangm@gmail.com>
2021-12-03 14:55:45 +0800

    refactor: remove outdated code

--- oomcli/cmd/get_meta_feature.go
+++ oomcli/cmd/get_meta_feature.go
@@ -2,15 +2,10 @@ package cmd
 
 import (
 	"context"
-	"encoding/csv"
 	"fmt"
 	"log"
-	"os"
-	"strconv"
 	"strings"
-	"time"
 
-	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v3"
 
@@ -66,21 +61,6 @@ func init() {
 	getMetaFeatureOpt.GroupName = flags.StringP("group", "g", "", "feature group")
 }
 
-func printFeatures(features types.FeatureList, output string, wide bool) error {
-	switch output {
-	case CSV:
-		return printFeaturesInCSV(features, wide)
-	case ASCIITable:
-		return printFeaturesInASCIITable(features, true, wide)
-	case Column:
-		return printFeaturesInASCIITable(features, false, wide)
-	case YAML:
-		return printFeatureInYaml(features)
-	default:
-		return fmt.Errorf("unsupported output format %s", output)
-	}
-}
-
 func printFeatureInYaml(features types.FeatureList) error {
 	var (
 		out   []byte
@@ -99,67 +79,4 @@ func printFeatureInYaml(features types.FeatureList) error {
 	}
 	fmt.Println(strings.Trim(string(out), "\n"))
 	return nil
-}
-
-func printFeaturesInCSV(features types.FeatureList, wide bool) error {
-	w := csv.NewWriter(os.Stdout)
-	if err := w.Write(featureHeader(wide)); err != nil {
-		return err
-	}
-	for _, feature := range features {
-		if err := w.Write(featureRecord(feature, wide)); err != nil {
-			return err
-		}
-	}
-
-	w.Flush()
-	return nil
-}
-
-func printFeaturesInASCIITable(features types.FeatureList, border, wide bool) error {
-	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader(featureHeader(wide))
-	table.SetAutoFormatHeaders(false)
-	table.SetAlignment(tablewriter.ALIGN_LEFT)
-	table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
-
-	if !border {
-		table.SetBorder(false)
-		table.SetHeaderLine(false)
-		table.SetNoWhiteSpace(true)
-		table.SetCenterSeparator("")
-		table.SetColumnSeparator("")
-		table.SetRowSeparator("")
-		table.SetTablePadding("  ")
-	}
-
-	for _, feature := range features {
-		table.Append(featureRecord(feature, wide))
-	}
-	table.Render()
-	return nil
-}
-
-func featureHeader(wide bool) []string {
-	if wide {
-		return []string{"ID", "NAME", "GROUP", "ENTITY", "CATEGORY", "DB-VALUE-TYPE", "VALUE-TYPE", "DESCRIPTION", "ONLINE-REVISION-ID", "CREATE-TIME", "MODIFY-TIME"}
-	}
-	return []string{"ID", "NAME", "GROUP", "ENTITY", "CATEGORY", "VALUE-TYPE", "DESCRIPTION"}
-}
-
-func featureRecord(f *types.Feature, wide bool) []string {
-	onlineRevisionID := "<NULL>"
-
-	if f.OnlineRevisionID() != nil {
-		onlineRevisionID = strconv.FormatInt(int64(*f.OnlineRevisionID()), 10)
-	}
-
-	if wide {
-		return []string{strconv.Itoa(f.ID), f.Name, f.Group.Name, f.Entity().Name, f.Group.Category, f.DBValueType, f.ValueType, f.Description, onlineRevisionID, f.CreateTime.Format(time.RFC3339), f.ModifyTime.Format(time.RFC3339)}
-	}
-	desc := f.Description
-	if len(desc) > MetadataFieldTruncateAt {
-		desc = fmt.Sprintf("%s...", desc[0:MetadataFieldTruncateAt])
-	}
-	return []string{strconv.Itoa(f.ID), f.Name, f.Group.Name, f.Entity().Name, f.Group.Category, f.ValueType, desc}
 }
