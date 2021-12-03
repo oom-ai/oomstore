7883e1349a4bc9fb92aaeaa05f37794789e85833
Jinghan Ying <jinghancc@gmail.com>
2021-12-03 11:23:25 +0800

    test(database/metadata): add unit tests for mysql feature

--- internal/database/metadata/mysql/db_test.go
+++ internal/database/metadata/mysql/db_test.go
@@ -37,3 +37,39 @@ func TestCreateGroup(t *testing.T) {
 func TestUpdateGroup(t *testing.T) {
 	test_impl.TestUpdateGroup(t, prepareStore)
 }
+
+func TestCreateFeature(t *testing.T) {
+	test_impl.TestCreateFeature(t, prepareStore)
+}
+
+func TestCreateFeatureWithSameName(t *testing.T) {
+	test_impl.TestCreateFeatureWithSameName(t, prepareStore)
+}
+
+func TestCreateFeatureWithSQLKeyword(t *testing.T) {
+	test_impl.TestCreateFeatureWithSQLKeyword(t, prepareStore)
+}
+
+func TestCreateFeatureWithInvalidDataType(t *testing.T) {
+	test_impl.TestCreateFeatureWithInvalidDataType(t, prepareStore)
+}
+
+func TestGetFeature(t *testing.T) {
+	test_impl.TestGetFeature(t, prepareStore)
+}
+
+func TestGetFeatureByName(t *testing.T) {
+	test_impl.TestGetFeatureByName(t, prepareStore)
+}
+
+func TestListFeature(t *testing.T) {
+	test_impl.TestListFeature(t, prepareStore)
+}
+
+func TestCacheListFeature(t *testing.T) {
+	test_impl.TestCacheListFeature(t, prepareStore)
+}
+
+func TestUpdateFeature(t *testing.T) {
+	test_impl.TestUpdateFeature(t, prepareStore)
+}
