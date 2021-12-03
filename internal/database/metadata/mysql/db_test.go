4e2cc2e88a027197b1b663cde74bb13d784fb5e6
Jinghan Ying <jinghancc@gmail.com>
2021-12-03 10:20:46 +0800

    feat(database/metadata): implement mysql group methods

--- internal/database/metadata/mysql/db_test.go
+++ internal/database/metadata/mysql/db_test.go
@@ -21,3 +21,19 @@ func TestUpdateEntity(t *testing.T) {
 func TestListEntity(t *testing.T) {
 	test_impl.TestListEntity(t, prepareStore)
 }
+
+func TestGetGroup(t *testing.T) {
+	test_impl.TestGetGroup(t, prepareStore)
+}
+
+func TestListGroup(t *testing.T) {
+	test_impl.TestListGroup(t, prepareStore)
+}
+
+func TestCreateGroup(t *testing.T) {
+	test_impl.TestCreateGroup(t, prepareStore)
+}
+
+func TestUpdateGroup(t *testing.T) {
+	test_impl.TestUpdateGroup(t, prepareStore)
+}
