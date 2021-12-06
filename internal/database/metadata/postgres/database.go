057b3d3c9c92cfd1809bf0bc403a86da76444965
groguyoda <groguyodabb@gmail.com>
2021-12-06 17:29:48 +0800

    refactor(database/metadata): move WithTransaction

--- internal/database/metadata/postgres/database.go
+++ internal/database/metadata/postgres/database.go
@@ -71,32 +71,3 @@ func (db *DB) Close() error {
 	}
 	return nil
 }
-
-func (db *DB) WithTransaction(ctx context.Context, fn func(context.Context, metadata.DBStore) error) (err error) {
-	tx, err := db.BeginTxx(ctx, nil)
-	if err != nil {
-		return
-	}
-
-	txStore := &Tx{Tx: tx}
-
-	defer func() {
-		if p := recover(); p != nil {
-			// a panic occurred, rollback and repanic
-			_ = tx.Rollback()
-			panic(p)
-		} else if err != nil {
-			// something went wrong, rollback
-			_ = tx.Rollback()
-		} else {
-			// all good, commit
-			err = tx.Commit()
-		}
-	}()
-
-	return fn(ctx, txStore)
-}
-
-func (tx *Tx) WithTransaction(ctx context.Context, fn func(context.Context, metadata.DBStore) error) (err error) {
-	return fn(ctx, tx)
-}
