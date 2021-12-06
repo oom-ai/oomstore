8f9db6c58909ddb1562329d6d972dffc061fa021
lianxmfor <1034552569@qq.com>
2021-12-06 15:21:35 +0800

    test(oomcli): add integration-test for items apply

--- oomcli/test/test_apply_feature.sh
+++ oomcli/test/test_apply_feature.sh
@@ -42,3 +42,62 @@ feature_expected='ID,NAME,GROUP,ENTITY,CATEGORY,VALUE-TYPE,DESCRIPTION
 '
 feature_actual=$(oomcli get meta feature -o csv)
 assert_eq "oomcli get meta feature" "$(sort <<< "$feature_expected")" "$(sort <<< "$feature_actual")"
+
+cat > "$TMPFILE" <<EOF
+kind: Entity
+name: user
+length: 8
+description: user ID
+---
+kind: Group
+name: account
+entity-name: user
+category: batch
+description: user account info
+---
+kind: Group
+name: transaction_stats
+entity-name: user
+category: batch
+description: user transaction statistics
+---
+items:
+  - kind: Feature
+    name: credit_score
+    group-name: account
+    db-value-type: int
+    description: "credit_score description"
+  - kind: Feature
+    name: account_age_days
+    group-name: account
+    db-value-type: int
+    description: "account_age_days description"
+  - kind: Feature
+    name: has_2fa_installed
+    group-name: account
+    db-value-type: bool
+    description: "has_2fa_installed description"
+  - kind: Feature
+    name: transaction_count_7d
+    group-name: transaction_stats
+    db-value-type: int
+    description: "transaction_count_7d description"
+  - kind: Feature
+    name: transaction_count_30d
+    group-name: transaction_stats
+    db-value-type: int
+    description: "transaction_count_30d description"
+EOF
+
+init_store
+oomcli apply -f "$TMPFILE"
+feature_expected='
+ID,NAME,GROUP,ENTITY,CATEGORY,VALUE-TYPE,DESCRIPTION
+1,credit_score,account,user,batch,int64,credit_score description
+2,account_age_days,account,user,batch,int64,account_age_days description
+3,has_2fa_installed,account,user,batch,bool,has_2fa_installed description
+4,transaction_count_7d,transaction_stats,user,batch,int64,transaction_count_7d description
+5,transaction_count_30d,transaction_stats,user,batch,int64,transaction_count_30d description
+'
+feature_actual=$(oomcli get meta feature -o csv)
+assert_eq "oomcli apply multiple features" "$(sort <<< "$feature_expected")" "$(sort <<< "$feature_actual")"
--- oomcli/test/test_apply_group.sh
+++ oomcli/test/test_apply_group.sh
@@ -81,3 +81,87 @@ feature_expected='ID,NAME,GROUP,ENTITY,CATEGORY,VALUE-TYPE,DESCRIPTION
 '
 feature_actual=$(oomcli get meta feature -o csv)
 assert_eq "oomcli get meta feature" "$(sort <<< "$feature_expected")" "$(sort <<< "$feature_actual")"
+
+init_store
+
+cat > "$TMPFILE" <<EOF
+kind: Entity
+name: user
+length: 8
+description: 'description'
+---
+kind: Entity
+name: device
+length: 8
+description: 'description'
+---
+items:
+  - kind: Group
+    name: account
+    entity-name: user
+    category: batch
+    description: user account info
+    features:
+      - name: state
+        db-value-type: varchar(32)
+        description: ""
+      - name: credit_score
+        db-value-type: int
+        description: credit_score description
+      - name: account_age_days
+        db-value-type: int
+        description: account_age_days description
+      - name: has_2fa_installed
+        db-value-type: bool
+        description: has_2fa_installed description
+  - kind: Group
+    name: transaction_stats
+    entity-name: user
+    category: batch
+    description: user transaction statistics
+    features:
+      - name: transaction_count_7d
+        db-value-type: int
+        description: transaction_count_7d description
+      - name: transaction_count_30d
+        db-value-type: int
+        description: transaction_count_30d description
+  - kind: Group
+    name: phone
+    entity-name: device
+    category: batch
+    description: phone info
+    features:
+      - name: model
+        db-value-type: varchar(32)
+        description: model description
+      - name: price
+        db-value-type: int
+        description: price description
+EOF
+
+oomcli apply -f "$TMPFILE"
+
+group_expected='
+ID,NAME,ENTITY,DESCRIPTION
+1,account,user,user account info
+2,transaction_stats,user,user transaction statistics
+3,phone,device,phone info
+'
+group_actual=$(oomcli get meta group -o csv --wide)
+filter() { cut -d ',' -f 1-4 <<<"$1"; }
+assert_eq "oomcli apply multiple group: check features" "$(filter "$group_expected" | sort)" "$(filter "$group_actual" | sort)"
+
+feature_expected='
+ID,NAME,GROUP,ENTITY,CATEGORY,VALUE-TYPE,DESCRIPTION
+1,state,account,user,batch,string,
+2,credit_score,account,user,batch,int64,credit_score description
+3,account_age_days,account,user,batch,int64,account_age_days description
+4,has_2fa_installed,account,user,batch,bool,has_2fa_installed description
+5,transaction_count_7d,transaction_stats,user,batch,int64,transaction_count_7d description
+6,transaction_count_30d,transaction_stats,user,batch,int64,transaction_count_30d description
+7,model,phone,device,batch,string,model description
+8,price,phone,device,batch,int64,price description
+'
+feature_actual=$(oomcli get meta feature -o csv)
+assert_eq "oomcli apply multiple group: check group" "$feature_expected" "$feature_actual"
