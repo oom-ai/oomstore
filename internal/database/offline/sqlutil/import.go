f2c1ed90138bca8e125a78893cdab33d4a56f171
Jinghan Ying <jinghancc@gmail.com>
2021-12-07 15:49:34 +0800

    feat(database/offline): copy offline method Import to sqlutil

---
+++ internal/database/offline/sqlutil/import.go
@@ -0,0 +1,75 @@
+package sqlutil
+
+import (
+	"context"
+	"encoding/csv"
+	"io"
+	"time"
+
+	"github.com/ethhte88/oomstore/internal/database/dbutil"
+	"github.com/ethhte88/oomstore/internal/database/offline"
+	"github.com/ethhte88/oomstore/pkg/oomstore/types"
+	"github.com/jmoiron/sqlx"
+	"github.com/lib/pq"
+)
+
+func Import(ctx context.Context, db *sqlx.DB, opt offline.ImportOpt) (int64, error) {
+	var revision int64
+	err := dbutil.WithTransaction(db, ctx, func(ctx context.Context, tx *sqlx.Tx) error {
+		// create the data table
+		schema, err := dbutil.BuildFeatureDataTableSchema(opt.DataTableName, opt.Entity, opt.Features, types.POSTGRES)
+		if err != nil {
+			return err
+		}
+		_, err = tx.ExecContext(ctx, schema)
+		if err != nil {
+			return err
+		}
+
+		// populate the data table
+		err = loadData(tx, ctx, opt.CsvReader, opt.DataTableName, opt.Header)
+		if err != nil {
+			return err
+		}
+
+		if opt.Revision != nil {
+			// use user-defined revision
+			revision = *opt.Revision
+		} else {
+			// generate revision using current timestamp
+			revision = time.Now().UnixMilli()
+		}
+		return nil
+	})
+	return revision, err
+}
+
+func loadData(tx *sqlx.Tx, ctx context.Context, csvReader *csv.Reader, tableName string, header []string) error {
+	stmt, err := tx.PreparexContext(ctx, pq.CopyIn(tableName, header...))
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	for {
+		row, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		args := []interface{}{}
+		for _, v := range row {
+			args = append(args, v)
+		}
+		if _, err := stmt.ExecContext(ctx, args...); err != nil {
+			return err
+		}
+	}
+
+	_, err = stmt.ExecContext(ctx)
+	return err
+
+}
