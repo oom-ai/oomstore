0618f2f6148c648f8f2fb0649602ad76dbb79dad
groguyoda <groguyodabb@gmail.com>
2021-12-07 14:58:24 +0800

    refactor(database/offline): modify offline Export method This commit refactors offline method Export, to make it adapt with mysql db.

--- internal/database/offline/postgres/export.go
+++ internal/database/offline/postgres/export.go
@@ -7,10 +7,11 @@ import (
 	"github.com/ethhte88/oomstore/internal/database/dbutil"
 	"github.com/ethhte88/oomstore/internal/database/offline"
 	"github.com/ethhte88/oomstore/pkg/oomstore/types"
+	"github.com/spf13/cast"
 )
 
 func (db *DB) Export(ctx context.Context, opt offline.ExportOpt) (<-chan types.ExportRecord, <-chan error) {
-	fields := append([]string{opt.EntityName}, opt.FeatureNames...)
+	fields := append([]string{opt.EntityName}, opt.Features.Names()...)
 	query := fmt.Sprintf("select %s from %s", dbutil.Quote(`"`, fields...), opt.DataTable)
 	if opt.Limit != nil {
 		query += fmt.Sprintf(" LIMIT %d", *opt.Limit)
@@ -21,7 +22,13 @@ func (db *DB) Export(ctx context.Context, opt offline.ExportOpt) (<-chan types.E
 	go func() {
 		defer close(stream)
 		defer close(errs)
-		rows, err := db.QueryxContext(ctx, query)
+		stmt, err := db.Preparex(query)
+		if err != nil {
+			errs <- err
+			return
+		}
+		defer stmt.Close()
+		rows, err := stmt.Queryx()
 		if err != nil {
 			errs <- err
 			return
@@ -32,6 +39,13 @@ func (db *DB) Export(ctx context.Context, opt offline.ExportOpt) (<-chan types.E
 			if err != nil {
 				errs <- fmt.Errorf("failed at rows.SliceScan, err=%v", err)
 				return
+			}
+			record[0] = cast.ToString(record[0])
+			for i, f := range opt.Features {
+				if f.ValueType != types.STRING {
+					continue
+				}
+				record[i+1] = cast.ToString(record[i+1])
 			}
 			stream <- record
 		}
