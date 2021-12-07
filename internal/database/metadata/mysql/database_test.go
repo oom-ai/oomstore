696f0576d489770ce13f1d874c1d49874b7ae9ed
Yik San Chan <evan.chanyiksan@gmail.com>
2021-12-07 16:46:47 +0800

    refactor: reuse OpenMysqlDB method from dbutil package

--- internal/database/metadata/mysql/database_test.go
+++ internal/database/metadata/mysql/database_test.go
@@ -5,6 +5,7 @@ import (
 	"sort"
 	"testing"
 
+	"github.com/ethhte88/oomstore/internal/database/dbutil"
 	"github.com/ethhte88/oomstore/internal/database/metadata"
 	"github.com/ethhte88/oomstore/internal/database/metadata/mysql"
 	"github.com/ethhte88/oomstore/internal/database/test/runtime_mysql"
@@ -19,7 +20,7 @@ func prepareStore(t *testing.T) (context.Context, metadata.Store) {
 func prepareDB(t *testing.T) (context.Context, *mysql.DB) {
 	ctx := context.Background()
 	opt := runtime_mysql.MySQLDbOpt
-	db, err := mysql.OpenDB(
+	db, err := dbutil.OpenMysqlDB(
 		opt.Host,
 		opt.Port,
 		opt.User,
