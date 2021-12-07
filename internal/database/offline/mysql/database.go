654898c31333dcf88652372771b109512a10b1c1
Jinghan Ying <jinghancc@gmail.com>
2021-12-07 14:11:21 +0800

    feat(database/offline): open offline mysql database

---
+++ internal/database/offline/mysql/database.go
@@ -0,0 +1,33 @@
+package mysql
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ethhte88/oomstore/internal/database/offline"
+	"github.com/ethhte88/oomstore/pkg/oomstore/types"
+	"github.com/jmoiron/sqlx"
+)
+
+var _ offline.Store = &DB{}
+
+type DB struct {
+	*sqlx.DB
+}
+
+func (db *DB) Ping(ctx context.Context) error {
+	return db.DB.PingContext(ctx)
+}
+
+func Open(option *types.MySQLOpt) (*DB, error) {
+	return OpenWith(option.Host, option.Port, option.User, option.Password, option.Database)
+}
+
+func OpenWith(host, port, user, password, database string) (*DB, error) {
+	db, err := sqlx.Open(
+		"mysql",
+		fmt.Sprintf("%s:%s@(%s:%s)/%s?parseTime=true", user, password, host, port, database))
+	return &DB{
+		DB: db,
+	}, err
+}
