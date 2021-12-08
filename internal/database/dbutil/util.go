0f938bfa0f69b5b42055ae6fa8f9a99500fa1073
Jinghan Ying <jinghancc@gmail.com>
2021-12-08 15:41:14 +0800

    refactor(database/dbutil):split some helpers to database.go

--- internal/database/dbutil/util.go
+++ internal/database/dbutil/util.go
@@ -3,64 +3,10 @@ package dbutil
 import (
 	"fmt"
 	"time"
-
-	"github.com/ethhte88/oomstore/pkg/oomstore/types"
-	"github.com/go-sql-driver/mysql"
-	"github.com/jackc/pgerrcode"
-	"github.com/jmoiron/sqlx"
-	"github.com/lib/pq"
 )
 
 type RowMap = map[string]interface{}
 
 func TempTable(prefix string) string {
 	return fmt.Sprintf("tmp_%s_%d", prefix, time.Now().UnixNano())
-}
-
-// Build MySQL data source name
-func OpenMysqlDB(host, port, user, password, database string) (*sqlx.DB, error) {
-	cfg := mysql.NewConfig()
-	cfg.Net = fmt.Sprintf("(%s:%s)", host, port)
-	cfg.User = user
-	cfg.Passwd = password
-	cfg.DBName = database
-	cfg.ParseTime = true
-
-	return sqlx.Open("mysql", cfg.FormatDSN())
-}
-
-func OpenPostgresDB(host, port, user, password, database string) (*sqlx.DB, error) {
-	return sqlx.Open(
-		"postgres",
-		fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
-			user,
-			password,
-			host,
-			port,
-			database),
-	)
-}
-
-func DeserializeString(i interface{}, backend types.BackendType) string {
-	switch backend {
-	case types.MYSQL:
-		return string(i.([]byte))
-	default:
-		return i.(string)
-	}
-}
-
-func IsTableNotFoundError(err error, backend types.BackendType) bool {
-	switch backend {
-	// https://dev.mysql.com/doc/mysql-errors/5.7/en/server-error-reference.html#error_er_no_such_table
-	case types.MYSQL:
-		if e2, ok := err.(*mysql.MySQLError); ok {
-			return e2.Number == 1146
-		}
-	case types.POSTGRES:
-		if e2, ok := err.(*pq.Error); ok {
-			return e2.Code == pgerrcode.UndefinedTable
-		}
-	}
-	return false
 }
