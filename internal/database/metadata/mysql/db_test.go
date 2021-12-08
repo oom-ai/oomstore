e9e8d3d0260146be3a6fec0f8274045947b4d892
Yik San Chan <evan.chanyiksan@gmail.com>
2021-12-08 16:59:47 +0800

    test: fix TestCreateDatabase

--- internal/database/metadata/mysql/db_test.go
+++ internal/database/metadata/mysql/db_test.go
@@ -2,12 +2,15 @@ package mysql_test
 
 import (
 	"context"
+	"sort"
 	"testing"
 
 	"github.com/ethhte88/oomstore/internal/database/metadata"
 	"github.com/ethhte88/oomstore/internal/database/metadata/mysql"
 	"github.com/ethhte88/oomstore/internal/database/metadata/test_impl"
 	"github.com/ethhte88/oomstore/internal/database/test/runtime_mysql"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func prepareStore() (context.Context, metadata.Store) {
@@ -27,6 +30,44 @@ func prepareStore() (context.Context, metadata.Store) {
 
 func TestPing(t *testing.T) {
 	test_impl.TestPing(t, prepareStore)
+}
+
+func TestCreateDatabase(t *testing.T) {
+	ctx, db := runtime_mysql.PrepareDB()
+	db.Close()
+
+	if err := mysql.CreateDatabase(ctx, runtime_mysql.MySQLDbOpt); err != nil {
+		panic(err)
+	}
+	store, err := mysql.Open(ctx, &runtime_mysql.MySQLDbOpt)
+	if err != nil {
+		panic(err)
+	}
+	defer store.Close()
+
+	var tables []string
+	err = store.SelectContext(ctx, &tables,
+		`SELECT table_name
+			FROM information_schema.tables
+			WHERE table_schema = 'test'
+			ORDER BY table_name;`)
+	require.NoError(t, err)
+
+	var wantTables []string
+	for table := range mysql.META_TABLE_SCHEMAS {
+		wantTables = append(wantTables, table)
+	}
+	for table := range mysql.META_VIEW_SCHEMAS {
+		wantTables = append(wantTables, table)
+	}
+
+	sort.Slice(tables, func(i, j int) bool {
+		return tables[i] < tables[j]
+	})
+	sort.Slice(wantTables, func(i, j int) bool {
+		return wantTables[i] < wantTables[j]
+	})
+	assert.Equal(t, wantTables, tables)
 }
 
 func TestCreateEntity(t *testing.T) {
