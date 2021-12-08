6a65eee8b183fe57dfd28c2e541606a5b0b5b40f
Jinghan Ying <jinghancc@gmail.com>
2021-12-08 11:02:36 +0800

    refactor(database/offline):merge postgres methods to one file

--- internal/database/offline/postgres/util.go
+++ internal/database/offline/postgres/util.go
@@ -5,16 +5,9 @@ import (
 	"encoding/csv"
 	"io"
 
-	"github.com/ethhte88/oomstore/internal/database/offline"
-	"github.com/ethhte88/oomstore/internal/database/offline/sqlutil"
-	"github.com/ethhte88/oomstore/pkg/oomstore/types"
 	"github.com/jmoiron/sqlx"
 	"github.com/lib/pq"
 )
-
-func (db *DB) Import(ctx context.Context, opt offline.ImportOpt) (int64, error) {
-	return sqlutil.Import(ctx, db.DB, opt, loadDataFromCSVReader, types.POSTGRES)
-}
 
 func loadDataFromCSVReader(tx *sqlx.Tx, ctx context.Context, csvReader *csv.Reader, tableName string, header []string) error {
 	stmt, err := tx.PreparexContext(ctx, pq.CopyIn(tableName, header...))
