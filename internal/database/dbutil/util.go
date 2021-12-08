6ddbe5dbdeaa820b8972c5c7c69dff2275634a2d
Jinghan Ying <jinghancc@gmail.com>
2021-12-08 15:37:15 +0800

    refactor(database/dbutil): split some helpers to create_table.go

--- internal/database/dbutil/util.go
+++ internal/database/dbutil/util.go
@@ -3,8 +3,6 @@ package dbutil
 import (
 	"context"
 	"fmt"
-	"sort"
-	"strconv"
 	"strings"
 	"time"
 
@@ -16,48 +14,6 @@ import (
 )
 
 type RowMap = map[string]interface{}
-
-const (
-	CREATE_DATA_TABLE_POSTGRES = `CREATE TABLE "{{TABLE_NAME}}" (
-	"{{ENTITY_NAME}}" VARCHAR({{ENTITY_LENGTH}}) PRIMARY KEY,
-	{{COLUMN_DEFS}});
-`
-	CREATE_DATA_TABLE_MYSQL = "CREATE TABLE `{{TABLE_NAME}}` ( " +
-		"`{{ENTITY_NAME}}` VARCHAR({{ENTITY_LENGTH}}) PRIMARY KEY," +
-		"{{COLUMN_DEFS}}); "
-)
-
-func BuildFeatureDataTableSchema(tableName string, entity *types.Entity, features types.FeatureList, backendType types.BackendType) (string, error) {
-	var columnFormat, tableSchema string
-	switch backendType {
-	case types.POSTGRES:
-		columnFormat = `"%s" %s`
-		tableSchema = CREATE_DATA_TABLE_POSTGRES
-	case types.MYSQL:
-		columnFormat = "`%s` %s"
-		tableSchema = CREATE_DATA_TABLE_MYSQL
-	default:
-		return "", fmt.Errorf("unsupported backend type %s", backendType)
-	}
-
-	// sort to ensure the schema looks consistent
-	sort.Slice(features, func(i, j int) bool {
-		return features[i].Name < features[j].Name
-	})
-	var columnDefs []string
-	for _, column := range features {
-		def := fmt.Sprintf(columnFormat, column.Name, column.DBValueType)
-		columnDefs = append(columnDefs, def)
-	}
-
-	// fill schema template
-	schema := strings.ReplaceAll(tableSchema, "{{TABLE_NAME}}", tableName)
-	schema = strings.ReplaceAll(schema, "{{ENTITY_NAME}}", entity.Name)
-	schema = strings.ReplaceAll(schema, "{{ENTITY_LENGTH}}", strconv.Itoa(entity.Length))
-	schema = strings.ReplaceAll(schema, "{{COLUMN_DEFS}}", strings.Join(columnDefs, ",\n"))
-
-	return schema, nil
-}
 
 func BuildConditions(equal map[string]interface{}, in map[string]interface{}) ([]string, []interface{}, error) {
 	cond := make([]string, 0)
