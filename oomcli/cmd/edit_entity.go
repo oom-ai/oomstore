b607f03b86f85dd254cf729ba41a119c4cbb4516
lianxmfor <1034552569@qq.com>
2021-12-08 12:00:57 +0800

    refactor(oomcli/edit_entity): make more clear

--- oomcli/cmd/edit_entity.go
+++ oomcli/cmd/edit_entity.go
@@ -5,8 +5,11 @@ import (
 	"log"
 	"os"
 
-	"github.com/ethhte88/oomstore/pkg/oomstore/types/apply"
 	"github.com/spf13/cobra"
+
+	"github.com/ethhte88/oomstore/pkg/oomstore"
+	"github.com/ethhte88/oomstore/pkg/oomstore/types"
+	"github.com/ethhte88/oomstore/pkg/oomstore/types/apply"
 )
 
 type editEntityOption struct {
@@ -30,27 +33,30 @@ var editEntityCmd = &cobra.Command{
 		oomStore := mustOpenOomStore(ctx, oomStoreCfg)
 		defer oomStore.Close()
 
-		tempFile, err := getTempFile()
+		entities, err := queryEntities(ctx, oomStore, editEntityOpt.entityName)
 		if err != nil {
 			log.Fatal(err)
 		}
-		if err := outputEntity(ctx, tempFile, YAML, oomStore, editEntityOpt.entityName); err != nil {
-			log.Fatal(err)
-		}
-		tempFileName := tempFile.Name()
-		tempFile.Close()
 
-		if err = openFileByEditor(ctx, tempFileName); err != nil {
-			log.Fatal(err)
-		}
-
-		tempFile, err = os.OpenFile(tempFileName, os.O_RDONLY, 0666)
+		fileName, err := writeEntitiesToTempFile(ctx, oomStore, entities)
 		if err != nil {
 			log.Fatal(err)
 		}
-		defer tempFile.Close()
 
-		if err := oomStore.Apply(ctx, apply.ApplyOpt{R: tempFile}); err != nil {
+		if err = openFileByEditor(ctx, fileName); err != nil {
+			log.Fatal(err)
+		}
+
+		file, err := os.OpenFile(fileName, os.O_RDONLY, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer func() {
+			file.Close()
+			os.Remove(file.Name())
+		}()
+
+		if err := oomStore.Apply(ctx, apply.ApplyOpt{R: file}); err != nil {
 			log.Fatalf("apply failed: %v", err)
 		}
 		log.Println("applied")
@@ -59,4 +65,18 @@ var editEntityCmd = &cobra.Command{
 
 func init() {
 	editCmd.AddCommand(editEntityCmd)
+}
+
+func writeEntitiesToTempFile(ctx context.Context, oomStore *oomstore.OomStore, entities types.EntityList) (string, error) {
+	tempFile, err := getTempFile()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tempFile.Close()
+
+	if err := serializeEntitiesToWriter(ctx, tempFile, oomStore, entities, YAML); err != nil {
+		return "", err
+	}
+
+	return tempFile.Name(), nil
 }
