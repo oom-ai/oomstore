9b72f7279c715850bc2eb5d21e5ffd6fe738a248
ethhte88 <ethhte88@gmail.com>
2021-12-08 22:24:21 +0800

    feat: support dynamodb online store Open

---
+++ internal/database/online/dynamodb/db.go
@@ -0,0 +1,24 @@
+package dynamodb
+
+import (
+	"context"
+
+	"github.com/ethhte88/oomstore/internal/database/dbutil"
+	"github.com/ethhte88/oomstore/internal/database/online"
+)
+
+func (db *DB) Get(ctx context.Context, opt online.GetOpt) (dbutil.RowMap, error) {
+	panic("implement me")
+}
+
+func (db *DB) MultiGet(ctx context.Context, opt online.MultiGetOpt) (map[string]dbutil.RowMap, error) {
+	panic("implement me")
+}
+
+func (db *DB) Import(ctx context.Context, opt online.ImportOpt) error {
+	panic("implement me")
+}
+
+func (db *DB) Purge(ctx context.Context, revisionID int) error {
+	panic("implement me")
+}
