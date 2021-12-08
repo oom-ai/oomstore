38288b222c1ee54b0f85a80071ff58b9c61d9478
groguyoda <groguyodabb@gmail.com>
2021-12-08 20:31:33 +0800

    feaet(database/offline): implement TypeTag for snowflake

---
+++ internal/database/offline/snowflake/type_tag.go
@@ -0,0 +1,57 @@
+package snowflake
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/ethhte88/oomstore/pkg/oomstore/types"
+)
+
+func TypeTag(dbType string) (string, error) {
+	var s = dbType
+	if pos := strings.Index(dbType, "("); pos != -1 {
+		fmt.Println(s[:pos])
+		s = s[:pos]
+	}
+	s = strings.TrimSpace(strings.ToLower(s))
+	if t, ok := typeMap[s]; !ok {
+		return "", fmt.Errorf("unsupported sql type: %s", dbType)
+	} else {
+		return t, nil
+	}
+}
+
+// TODO: add type NUMBER, DECIMAL, NUMERIC
+var (
+	typeMap = map[string]string{
+		"boolean": types.BOOL,
+
+		"binary":    types.BYTES,
+		"varbinary": types.BYTES,
+
+		"integer":  types.INT64,
+		"int":      types.INT64,
+		"smallint": types.INT64,
+		"bigint":   types.INT64,
+		"tinyint":  types.INT64,
+		"byteint":  types.INT64,
+
+		"double":           types.FLOAT64,
+		"double precision": types.FLOAT64,
+		"real":             types.FLOAT64,
+		"float":            types.FLOAT64,
+		"float4":           types.FLOAT64,
+		"float8":           types.FLOAT64,
+
+		"string":    types.STRING,
+		"text":      types.STRING,
+		"varchar":   types.STRING,
+		"char":      types.STRING,
+		"character": types.STRING,
+
+		"date":      types.TIME,
+		"time":      types.TIME,
+		"datetime":  types.TIME,
+		"timestamp": types.TIME,
+	}
+)
