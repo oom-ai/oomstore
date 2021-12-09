b52c355f9fdd4c250847d19f78094d0f4b8f101f
Jinghan Ying <jinghancc@gmail.com>
2021-12-09 17:33:11 +0800

    test(database/online): add unit test for Import, Get, MultiGet

--- internal/database/online/test_impl/util.go
+++ internal/database/online/test_impl/util.go
@@ -44,13 +44,27 @@ func init() {
 					ValueType:   types.STRING,
 					DBValueType: "varchar(1)",
 				},
+				&types.Feature{
+					ID:          3,
+					Name:        "account",
+					GroupID:     1,
+					ValueType:   types.FLOAT64,
+					DBValueType: "double",
+				},
+				&types.Feature{
+					ID:          3,
+					Name:        "is_active",
+					GroupID:     1,
+					ValueType:   types.BOOL,
+					DBValueType: "bool",
+				},
 			},
 			Revision: &types.Revision{ID: 3, GroupID: 1},
 			Entity:   &types.Entity{ID: 5, Name: "user", Length: 4},
 			Data: []types.ExportRecord{
-				[]interface{}{"3215", int64(18), "F"},
-				[]interface{}{"3216", int64(29), nil},
-				[]interface{}{"3217", int64(44), "M"},
+				[]interface{}{"3215", int64(18), "F", 1.1, true},
+				[]interface{}{"3216", int64(29), nil, 2.0, false},
+				[]interface{}{"3217", int64(44), "M", 3.1, true},
 			},
 		}
 
