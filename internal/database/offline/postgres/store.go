9766e0ab573fd9e2508aa1f6ee34db2285a5d97c
winksyll <yllwinks@gmail.com>
2021-12-09 17:48:10 +0800

    feat: implement TableSchema for postgres

--- internal/database/offline/postgres/store.go
+++ internal/database/offline/postgres/store.go
@@ -40,3 +40,28 @@ func (db *DB) Join(ctx context.Context, opt offline.JoinOpt) (*types.JoinResult,
 func (db *DB) TypeTag(dbType string) (string, error) {
 	return TypeTag(dbType)
 }
+
+func (db *DB) TableSchema(ctx context.Context, tableName string) (*types.DataTableSchema, error) {
+	rows, err := db.QueryxContext(ctx, "select column_name, data_type from information_schema.columns where table_name = $1", tableName)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var schema types.DataTableSchema
+	for rows.Next() {
+		var fieldName, dbValueType string
+		if err := rows.Scan(&fieldName, &dbValueType); err != nil {
+			return nil, err
+		}
+		valueType, err := db.TypeTag(dbValueType)
+		if err != nil {
+			return nil, err
+		}
+		schema.Fields = append(schema.Fields, types.DataTableFieldSchema{
+			Name:      fieldName,
+			ValueType: valueType,
+		})
+	}
+	return &schema, nil
+}
