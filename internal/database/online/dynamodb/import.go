19774656dccc9b467432ef180df42eaf1279deb8
Jinghan Ying <jinghancc@gmail.com>
2021-12-09 17:30:55 +0800

    feat(database/online): implement method Import for dynamoDB

---
+++ internal/database/online/dynamodb/import.go
@@ -0,0 +1,106 @@
+package dynamodb
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/ethhte88/oomstore/internal/database/online"
+	"github.com/ethhte88/oomstore/internal/database/online/sqlutil"
+	oomTypes "github.com/ethhte88/oomstore/pkg/oomstore/types"
+)
+
+const (
+	BatchWriteItemCapacity = 25
+)
+
+func (db *DB) Import(ctx context.Context, opt online.ImportOpt) error {
+	// Step 1: create table
+	tableName := sqlutil.OnlineTableName(opt.Revision.ID)
+	_, err := db.CreateTable(ctx, &dynamodb.CreateTableInput{
+		TableName: aws.String(tableName),
+		KeySchema: []types.KeySchemaElement{
+			{
+				AttributeName: aws.String(opt.Entity.Name),
+				KeyType:       types.KeyTypeHash,
+			},
+		},
+		AttributeDefinitions: []types.AttributeDefinition{
+			{
+				AttributeName: aws.String(opt.Entity.Name),
+				AttributeType: types.ScalarAttributeTypeS,
+			},
+		},
+		ProvisionedThroughput: &types.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(10),
+			WriteCapacityUnits: aws.Int64(10),
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	// Step 2: import items to the table
+	items := make([]types.WriteRequest, 0, BatchWriteItemCapacity)
+	for record := range opt.ExportStream {
+		item, err := buildItem(record, opt)
+		if err != nil {
+			return err
+		}
+		items = append(items, types.WriteRequest{
+			PutRequest: &types.PutRequest{
+				Item: item,
+			},
+		})
+		if len(items) == BatchWriteItemCapacity {
+			if err = batchWrite(ctx, db, tableName, items); err != nil {
+				return err
+			}
+			items = make([]types.WriteRequest, 0, BatchWriteItemCapacity)
+		}
+	}
+	if err = batchWrite(ctx, db, tableName, items); err != nil {
+		return err
+	}
+
+	if opt.ExportError != nil {
+		return <-opt.ExportError
+	}
+	return nil
+}
+
+func batchWrite(ctx context.Context, db *DB, tableName string, items []types.WriteRequest) error {
+	if len(items) == 0 {
+		return nil
+	}
+	_, err := db.BatchWriteItem(ctx, &dynamodb.BatchWriteItemInput{
+		RequestItems: map[string][]types.WriteRequest{
+			tableName: items,
+		},
+	})
+	return err
+}
+
+func buildItem(record oomTypes.ExportRecord, opt online.ImportOpt) (map[string]types.AttributeValue, error) {
+	item := make(map[string]types.AttributeValue)
+	entityKeyValue, err := attributevalue.Marshal(record.EntityKey())
+	if err != nil {
+		return nil, err
+	}
+	item[opt.Entity.Name] = entityKeyValue
+
+	for i, feature := range opt.FeatureList {
+		value, err := serializeByTag(record.ValueAt(i), feature.ValueType)
+		if err != nil {
+			return nil, err
+		}
+		attributeValue, err := attributevalue.Marshal(value)
+		if err != nil {
+			return nil, err
+		}
+		item[feature.Name] = attributeValue
+	}
+	return item, nil
+}
