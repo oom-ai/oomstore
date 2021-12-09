35d4022073d5b6533c92576f4c16f3147f5b08cf
Yik San Chan <evan.chanyiksan@gmail.com>
2021-12-09 12:09:46 +0800

    refactor: lift Open method

--- internal/database/online/mysql/db.go
+++ internal/database/online/mysql/db.go
@@ -19,13 +19,13 @@ type DB struct {
 	*sqlx.DB
 }
 
-func (db *DB) Ping(ctx context.Context) error {
-	return db.DB.PingContext(ctx)
-}
-
 func Open(opt *types.MySQLOpt) (*DB, error) {
 	db, err := dbutil.OpenMysqlDB(opt.Host, opt.Port, opt.User, opt.Password, opt.Database)
 	return &DB{db}, err
+}
+
+func (db *DB) Ping(ctx context.Context) error {
+	return db.DB.PingContext(ctx)
 }
 
 func (db *DB) Get(ctx context.Context, opt online.GetOpt) (dbutil.RowMap, error) {
