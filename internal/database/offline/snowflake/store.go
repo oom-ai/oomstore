e9f20e48c7878c84a219b69ccae679e9367879cb
Yik San Chan <evan.chanyiksan@gmail.com>
2021-12-09 17:21:09 +0800

    chore: reorg offline package files

--- internal/database/offline/snowflake/store.go
+++ internal/database/offline/snowflake/store.go
@@ -7,9 +7,40 @@ import (
 	"github.com/ethhte88/oomstore/internal/database/offline"
 	"github.com/ethhte88/oomstore/internal/database/offline/sqlutil"
 	"github.com/ethhte88/oomstore/pkg/oomstore/types"
+	"github.com/jmoiron/sqlx"
+	"github.com/snowflakedb/gosnowflake"
 )
 
 const SnowflakeBatchSize = 100
+
+var _ offline.Store = &DB{}
+
+type DB struct {
+	*sqlx.DB
+}
+
+func (db *DB) Ping(ctx context.Context) error {
+	return db.DB.PingContext(ctx)
+}
+
+func Open(opt *types.SnowflakeOpt) (*DB, error) {
+	dsn, err := gosnowflake.DSN(&gosnowflake.Config{
+		Account:  opt.Account,
+		User:     opt.User,
+		Password: opt.Password,
+		Database: opt.Database,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := sqlx.Open("snowflake", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DB{DB: db}, err
+}
 
 func (db *DB) Import(ctx context.Context, opt offline.ImportOpt) (int64, error) {
 	return sqlutil.Import(ctx, db.DB, opt, dbutil.LoadDataFromCSVReader(types.SNOWFLAKE, SnowflakeBatchSize), types.SNOWFLAKE)
