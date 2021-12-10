99fc4528d17b11199bff0f79397b7752e0e81e1f
lianxmfor <1034552569@qq.com>
2021-12-10 17:37:08 +0800

    feat(online/cassandra): implement get and multiple get

--- internal/database/online/cassandra/db.go
+++ internal/database/online/cassandra/db.go
@@ -2,23 +2,7 @@ package cassandra
 
 import (
 	"context"
-	"fmt"
-	"strings"
-
-	"github.com/gocql/gocql"
-
-	"github.com/ethhte88/oomstore/internal/database/dbutil"
-	"github.com/ethhte88/oomstore/internal/database/online"
-	"github.com/ethhte88/oomstore/internal/database/online/sqlutil"
 )
-
-func (db *DB) Get(ctx context.Context, opt online.GetOpt) (dbutil.RowMap, error) {
-	panic("implement me!")
-}
-
-func (db *DB) MultiGet(ctx context.Context, opt online.MultiGetOpt) (map[string]dbutil.RowMap, error) {
-	panic("implement me!")
-}
 
 func (db *DB) Purge(ctx context.Context, revisionID int) error {
 	panic("implement me!")
