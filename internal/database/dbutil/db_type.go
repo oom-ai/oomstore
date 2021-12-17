f0d8596ed8b49a550ca263a16d9643caa1a2628b
lianxmfor <1034552569@qq.com>
2021-12-17 11:13:14 +0800

    feat(dbuti/dbtype): add mysql/postgres/snoflake/dynamodb

--- internal/database/dbutil/db_type.go
+++ internal/database/dbutil/db_type.go
@@ -10,10 +10,18 @@ import (
 func GetDbTypeFrom(backend types.BackendType, valueType string) (string, error) {
 	var mp map[string]string
 	switch backend {
-	case types.POSTGRES, types.REDIS, types.MYSQL, types.SNOWFLAKE, types.DYNAMODB:
-		return "", errdefs.InvalidAttribute(fmt.Errorf("unsupported backend: %s", backend))
+	case types.POSTGRES:
+		mp = postgresType
+	case types.MYSQL:
+		mp = mysqlType
 	case types.CASSANDRA:
 		mp = cassandraType
+	case types.SNOWFLAKE:
+		mp = snowFlake
+	case types.DYNAMODB:
+		mp = dynamoDB
+	default:
+		return "", errdefs.InvalidAttribute(fmt.Errorf("unsupported backend: %s", backend))
 	}
 
 	t, ok := mp[valueType]
@@ -24,6 +32,38 @@ func GetDbTypeFrom(backend types.BackendType, valueType string) (string, error) 
 }
 
 var (
+	mysqlType = map[string]string{
+		types.STRING:  "text",
+		types.INT64:   "bigint",
+		types.FLOAT64: "double",
+		types.BOOL:    "bool",
+		types.TIME:    "datetime",
+		types.BYTES:   "binary",
+	}
+	postgresType = map[string]string{
+		types.STRING:  "text",
+		types.INT64:   "bigint",
+		types.FLOAT64: "double precision",
+		types.BOOL:    "boolean",
+		types.TIME:    "timestamptz",
+		types.BYTES:   "bytea",
+	}
+	snowFlake = map[string]string{
+		types.STRING:  "VARCHAR",
+		types.INT64:   "INTEGER",
+		types.FLOAT64: "DOUBLE",
+		types.BOOL:    "BOOLEAN",
+		types.TIME:    "TIME",
+		types.BYTES:   "BINARY",
+	}
+	dynamoDB = map[string]string{
+		types.STRING:  "String",
+		types.INT64:   "BigInteger",
+		types.FLOAT64: "Float",
+		types.BOOL:    "Boolean",
+		types.TIME:    "Date",
+		types.BYTES:   "Byte",
+	}
 	cassandraType = map[string]string{
 		types.STRING:  "text",
 		types.INT64:   "bigint",
