86c825ee5c51fb5d67c0888f5d4bb12c3012e32f
jinghan <jinghancc@gmail.com>
2021-12-21 19:58:27 +0800

    test(database/metadata): add unit test for SQLite methods

--- internal/database/metadata/sqlite/db_test.go
+++ internal/database/metadata/sqlite/db_test.go
@@ -84,3 +84,79 @@ func TestUpdateEntity(t *testing.T) {
 func TestListEntity(t *testing.T) {
 	test_impl.TestListEntity(t, prepareStore)
 }
+
+func TestCreateGroup(t *testing.T) {
+	test_impl.TestCreateGroup(t, prepareStore)
+}
+
+func TestUpdateGroup(t *testing.T) {
+	test_impl.TestUpdateGroup(t, prepareStore)
+}
+
+func TestGetGroup(t *testing.T) {
+	test_impl.TestGetGroup(t, prepareStore)
+}
+
+func TestListGroup(t *testing.T) {
+	test_impl.TestListGroup(t, prepareStore)
+}
+
+func TestCreateFeature(t *testing.T) {
+	test_impl.TestCreateFeature(t, prepareStore)
+}
+
+func TestCreateFeatureWithSameName(t *testing.T) {
+	test_impl.TestCreateFeatureWithSameName(t, prepareStore)
+}
+
+func TestCreateFeatureWithSQLKeyword(t *testing.T) {
+	test_impl.TestCreateFeatureWithSQLKeyword(t, prepareStore)
+}
+
+func TestCreateFeatureWithInvalidDataType(t *testing.T) {
+	// According to SQLite Type Affinity, invalid data type will be regarded as Numeric,
+	// so we cannot use `Create Table` to check whether data type is valid or not.
+	// This issue will be auto-resolved when we infer DB value type from go value type.
+	t.Skip()
+	test_impl.TestCreateFeatureWithInvalidDataType(t, prepareStore)
+}
+
+func TestUpdateFeature(t *testing.T) {
+	test_impl.TestUpdateFeature(t, prepareStore)
+}
+
+func TestGetFeature(t *testing.T) {
+	test_impl.TestGetFeature(t, prepareStore)
+}
+
+func TestGetFeatureByName(t *testing.T) {
+	test_impl.TestGetFeatureByName(t, prepareStore)
+}
+
+func TestListFeature(t *testing.T) {
+	test_impl.TestListFeature(t, prepareStore)
+}
+
+func TestCacheListFeature(t *testing.T) {
+	test_impl.TestCacheListFeature(t, prepareStore)
+}
+
+func TestCreateRevision(t *testing.T) {
+	test_impl.TestCreateRevision(t, prepareStore)
+}
+
+func TestUpdateRevision(t *testing.T) {
+	test_impl.TestUpdateRevision(t, prepareStore)
+}
+
+func TestGetRevision(t *testing.T) {
+	test_impl.TestGetRevision(t, prepareStore)
+}
+
+func TestGetRevisionBy(t *testing.T) {
+	test_impl.TestGetRevisionBy(t, prepareStore)
+}
+
+func TestListRevision(t *testing.T) {
+	test_impl.TestListRevision(t, prepareStore)
+}
