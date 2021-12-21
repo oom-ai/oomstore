7c7e74485427b590d074ce4224fd017fdbfa6073
jinghan <jinghancc@gmail.com>
2021-12-21 19:57:29 +0800

    feate(database/metadata): implement createFeature for SQLite

---
+++ internal/database/metadata/sqlite/feature.go
@@ -0,0 +1,29 @@
+package sqlite
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/mattn/go-sqlite3"
+
+	"github.com/oom-ai/oomstore/internal/database/metadata"
+)
+
+func createFeature(ctx context.Context, sqlxCtx metadata.SqlxContext, opt metadata.CreateFeatureOpt) (int, error) {
+	query := "INSERT INTO feature(name, group_id, db_value_type, value_type, description) VALUES (?, ?, ?, ?, ?)"
+	res, err := sqlxCtx.ExecContext(ctx, sqlxCtx.Rebind(query), opt.FeatureName, opt.GroupID, opt.DBValueType, opt.ValueType, opt.Description)
+	if err != nil {
+		if sqliteErr, ok := err.(sqlite3.Error); ok {
+			if sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
+				return 0, fmt.Errorf("feature %s already exists", opt.FeatureName)
+			}
+		}
+		return 0, err
+	}
+
+	featureID, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return int(featureID), err
+}
