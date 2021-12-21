85ba60c5aba7e31b96b7f7e47fa34947e7e766f4
Yik San Chan <evan.chanyiksan@gmail.com>
2021-12-21 23:15:44 +0800

    refactor: cleanup TypeTag

--- internal/database/offline/sqlite/type_tag.go
+++ internal/database/offline/sqlite/type_tag.go
@@ -1,23 +1,12 @@
 package sqlite
 
 import (
-	"fmt"
-	"strings"
-
+	"github.com/oom-ai/oomstore/internal/database/offline/sqlutil"
 	"github.com/oom-ai/oomstore/pkg/oomstore/types"
 )
 
 func TypeTag(dbType string) (string, error) {
-	var s = dbType
-	if pos := strings.Index(dbType, "("); pos != -1 {
-		s = s[:pos]
-	}
-	s = strings.TrimSpace(strings.ToLower(s))
-	if t, ok := typeMap[s]; !ok {
-		return "", fmt.Errorf("unsupported sql type: %s", dbType)
-	} else {
-		return t, nil
-	}
+	return sqlutil.TypeTag(typeMap, dbType)
 }
 
 var (
