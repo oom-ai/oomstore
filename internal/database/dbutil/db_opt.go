154c245345f8498a77b9ed32dd54a5be955f1e13
Jinghan Ying <jinghancc@gmail.com>
2021-12-22 19:08:12 +0800

    feat(database): define DBOpt, add method ExecContext and BuildInsertQuery

---
+++ internal/database/dbutil/db_opt.go
@@ -0,0 +1,68 @@
+package dbutil
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cast"
+
+	"cloud.google.com/go/bigquery"
+	"github.com/jmoiron/sqlx"
+	"github.com/oom-ai/oomstore/pkg/oomstore/types"
+)
+
+type DBOpt struct {
+	Backend types.BackendType
+
+	// Sqlx
+	SqlxDB *sqlx.DB
+
+	// BigQuery
+	BigQueryDB *bigquery.Client
+	DatasetID  *string
+}
+
+func (d *DBOpt) ExecContext(ctx context.Context, query string, args []interface{}) error {
+	switch d.Backend {
+	case types.BIGQUERY:
+		_, err := d.BigQueryDB.Query(query).Read(ctx)
+		return err
+	default:
+		_, err := d.SqlxDB.ExecContext(ctx, d.SqlxDB.Rebind(query), args...)
+		return err
+	}
+}
+
+func (d *DBOpt) BuildInsertQuery(tableName string, records []interface{}, columns []string) (string, []interface{}, error) {
+	if len(records) == 0 {
+		return "", nil, nil
+	}
+
+	var columnStr string
+	switch d.Backend {
+	case types.POSTGRES, types.SNOWFLAKE, types.REDSHIFT:
+		columnStr = Quote(`"`, columns...)
+		tableName = fmt.Sprintf(`"%s"`, tableName)
+	case types.MYSQL, types.SQLite, types.BIGQUERY:
+		columnStr = Quote("`", columns...)
+		tableName = fmt.Sprintf("`%s`", tableName)
+	}
+
+	switch d.Backend {
+	case types.BIGQUERY:
+		values := make([]string, 0, len(records))
+		for _, row := range records {
+			values = append(values, fmt.Sprintf("(%s)", strings.Join(cast.ToStringSlice(row), ",")))
+		}
+		return fmt.Sprintf(`INSERT INTO %s.%s (%s) VALUES %s`, *d.DatasetID, tableName, columnStr, strings.Join(values, ",")), nil, nil
+	default:
+		valueFlags := make([]string, 0, len(records))
+		for i := 0; i < len(records); i++ {
+			valueFlags = append(valueFlags, "(?)")
+		}
+		return sqlx.In(
+			fmt.Sprintf(`INSERT INTO %s (%s) VALUES %s`, tableName, columnStr, strings.Join(valueFlags, ",")),
+			records...)
+	}
+}
