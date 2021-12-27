31911b6183288806caedb4b76e489881cfe3c313
Jinghan Ying <jinghancc@gmail.com>
2021-12-27 17:20:04 +0800

    refactor(pkg/oomstore): use FeatureFullNames for API OnlineGet

--- pkg/oomstore/types/feature_values.go
+++ pkg/oomstore/types/feature_values.go
@@ -1,15 +1,15 @@
 package types
 
 type FeatureValues struct {
-	EntityName      string
-	EntityKey       string
-	FeatureNames    []string
-	FeatureValueMap map[string]interface{}
+	EntityName       string
+	EntityKey        string
+	FeatureFullNames []string
+	FeatureValueMap  map[string]interface{}
 }
 
 func (fv *FeatureValues) FeatureValueSlice() []interface{} {
-	values := make([]interface{}, 0, len(fv.FeatureNames))
-	for _, name := range fv.FeatureNames {
+	values := make([]interface{}, 0, len(fv.FeatureFullNames))
+	for _, name := range fv.FeatureFullNames {
 		values = append(values, fv.FeatureValueMap[name])
 	}
 	return values
