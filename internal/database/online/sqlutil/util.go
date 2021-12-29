5d8ff191697f79ddc04ec608d9a28991d5364b43
Yik San Chan <evan.chanyiksan@gmail.com>
2021-12-29 18:18:57 +0800

    refactor: move deserializeByTag

--- internal/database/online/sqlutil/util.go
+++ internal/database/online/sqlutil/util.go
@@ -21,33 +21,6 @@ func OnlineStreamTableName(groupID int) string {
 	return fmt.Sprintf("online_stream_%d", groupID)
 }
 
-func deserializeByTag(i interface{}, valueType types.ValueType, backend types.BackendType) (interface{}, error) {
-	if i == nil {
-		return nil, nil
-	}
-
-	switch valueType {
-	case types.String:
-		if backend == types.BackendMySQL {
-			return string(i.([]byte)), nil
-		}
-		return i, nil
-	case types.Bool:
-		if backend == types.BackendMySQL || backend == types.BackendSQLite {
-			if i == int64(1) {
-				return true, nil
-			} else if i == int64(0) {
-				return false, nil
-			} else {
-				return nil, fmt.Errorf("invalid bool value: %s", i)
-			}
-		}
-		return i, nil
-	default:
-		return i, nil
-	}
-}
-
 func CreateStreamTableSchema(ctx context.Context, tableName string, entity *types.Entity, backend types.BackendType) (string, error) {
 	var (
 		entityFormat string
