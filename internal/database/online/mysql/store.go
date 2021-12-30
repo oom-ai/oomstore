e898308aa59bbb266e7bcf8d834ec8184326f275
lianxmfor <1034552569@qq.com>
2021-12-30 13:32:17 +0800

    feat(online/mysql): implement push method

--- internal/database/online/mysql/store.go
+++ internal/database/online/mysql/store.go
@@ -2,6 +2,8 @@ package mysql
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
@@ -44,8 +46,36 @@ func (db *DB) Purge(ctx context.Context, revisionID int) error {
 	return sqlutil.Purge(ctx, db.DB, revisionID, BackendType)
 }
 
+// TODO: refactor with text/template
 func (db *DB) Push(ctx context.Context, opt online.PushOpt) error {
-	panic("Implement me!")
+	tableName := sqlutil.OnlineStreamTableName(opt.GroupID)
+
+	insertColumns := append([]string{opt.Entity.Name}, opt.FeatureNames...)
+	insertColumnPlaceholders := make([]string, 0, len(insertColumns))
+	for i := 0; i < len(insertColumnPlaceholders); i++ {
+		insertColumnPlaceholders = append(insertColumnPlaceholders, "?")
+	}
+
+	insertValues := append([]interface{}{opt.EntityKey}, opt.FeatureValues...)
+	insertValuePlaceholders := make([]string, 0, len(insertValues))
+	for i := 0; i < len(insertValues); i++ {
+		insertValuePlaceholders = append(insertValuePlaceholders, "?")
+	}
+
+	updateValues := opt.FeatureValues
+	updatePlaceholders := make([]string, 0, len(opt.FeatureNames))
+	for _, name := range opt.FeatureNames {
+		updatePlaceholders = append(updatePlaceholders, fmt.Sprintf("%s=?", name))
+	}
+
+	query := fmt.Sprintf(`INSERT INTO %s (%s) VALUES(%s) ON DUPLICATE KEY UPDATE %s`,
+		tableName,
+		strings.Join(insertColumns, ","),
+		strings.Join(insertValuePlaceholders, ","),
+		strings.Join(updatePlaceholders, ","),
+	)
+	_, err := db.ExecContext(ctx, query, append(insertValues, updateValues...)...)
+	return err
 }
 
 func (db *DB) PrepareStreamTable(ctx context.Context, opt online.PrepareStreamTableOpt) error {
