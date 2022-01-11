6afe0bb54e87fdcad767516b067a3579e4875184
Jinghan Ying <jinghancc@gmail.com>
2022-01-11 17:06:59 +0800

    test(database/offline): add unit test for export streaming features

--- internal/database/offline/test_impl/export.go
+++ internal/database/offline/test_impl/export.go
@@ -2,6 +2,7 @@ package test_impl
 
 import (
 	"bufio"
+	"fmt"
 	"strings"
 	"testing"
 
@@ -16,7 +17,12 @@ func TestExport(t *testing.T, prepareStore PrepareStoreFn, destroyStore DestroyS
 	ctx, store := prepareStore(t)
 	defer store.Close()
 
-	snapshotTable := "offline_1_1"
+	snapshotTable := "offline_snapshot_1_1"
+	cdcTable := "offline_cdc_1_1"
+	unixMilli := &types.Feature{
+		Name:      "unix_milli",
+		ValueType: types.Int64,
+	}
 	features := []*types.Feature{
 		{
 			Name:      "model",
@@ -35,11 +41,21 @@ func TestExport(t *testing.T, prepareStore PrepareStoreFn, destroyStore DestroyS
 `)),
 		Delimiter: ",",
 	})
+	buildTestSnapshotTable(ctx, t, store, append(features, unixMilli), 1, cdcTable, &offline.CSVSource{
+		Reader: bufio.NewReader(strings.NewReader(`1234,xiaomi-1,120,2
+1235,apple-2,115,5
+1234,xiaomi-1,130,10
+1238,galaxy,100,11
+1239,galaxy,90,12
+`)),
+		Delimiter: ",",
+	})
 
 	testCases := []struct {
-		description string
-		opt         offline.ExportOpt
-		expected    [][]interface{}
+		description   string
+		opt           offline.ExportOpt
+		expected      [][]interface{}
+		expectedError error
 	}{
 		{
 			description: "no features",
@@ -51,13 +67,39 @@ func TestExport(t *testing.T, prepareStore PrepareStoreFn, destroyStore DestroyS
 			expected: [][]interface{}{{"1234"}, {"1235"}, {"1236"}, {"1237"}},
 		},
 		{
-			description: "valid features and valid entity rows",
+			description: "invalid option",
+			opt: offline.ExportOpt{
+				SnapshotTable: snapshotTable,
+				CdcTable:      &cdcTable,
+				EntityName:    "device",
+				Features:      features,
+			},
+			expectedError: fmt.Errorf("invalid option %+v", offline.ExportOpt{
+				SnapshotTable: snapshotTable,
+				CdcTable:      &cdcTable,
+				EntityName:    "device",
+				Features:      features,
+			}),
+		},
+		{
+			description: "batch features",
 			opt: offline.ExportOpt{
 				SnapshotTable: snapshotTable,
 				EntityName:    "device",
 				Features:      features,
 			},
 			expected: [][]interface{}{{"1234", "xiaomi", int64(100)}, {"1235", "apple", int64(200)}, {"1236", "huawei", int64(300)}, {"1237", "oneplus", int64(240)}},
+		},
+		{
+			description: "streaming features",
+			opt: offline.ExportOpt{
+				SnapshotTable: snapshotTable,
+				CdcTable:      &cdcTable,
+				UnixMilli:     int64Ptr(11),
+				EntityName:    "device",
+				Features:      features,
+			},
+			expected: [][]interface{}{{"1234", "xiaomi-1", int64(130)}, {"1235", "apple-2", int64(115)}, {"1236", "huawei", int64(300)}, {"1237", "oneplus", int64(240)}, {"1238", "galaxy", int64(100)}},
 		},
 	}
 
@@ -68,8 +110,16 @@ func TestExport(t *testing.T, prepareStore PrepareStoreFn, destroyStore DestroyS
 			for row := range actual {
 				values = append(values, row)
 			}
-			assert.Equal(t, tc.expected, values)
-			assert.NoError(t, <-errs)
+			if tc.expectedError != nil {
+				assert.EqualError(t, <-errs, tc.expectedError.Error())
+			} else {
+				assert.ElementsMatch(t, tc.expected, values)
+				assert.NoError(t, <-errs)
+			}
 		})
 	}
+}
+
+func int64Ptr(i int64) *int64 {
+	return &i
 }
