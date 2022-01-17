3591332f9cd0509d8a31357c792b76dd92b7e382
Wenxuan Zhang <wenxuangm@gmail.com>
2022-01-17 18:16:10 +0800

    refactor(sdk/rust): impl trait to convert from `Value` to Kind

--- sdk/rust/src/util.rs
+++ sdk/rust/src/util.rs
@@ -1,17 +1,16 @@
 use crate::oomagent::{value, FeatureValueMap, Value};
 use std::collections::HashMap;
 
+impl From<Value> for value::Kind {
+    fn from(value: Value) -> Self {
+        value.kind.expect("`oneof` in protobuffer should not be none")
+    }
+}
+
 pub fn parse_raw_feature_values(input: FeatureValueMap) -> HashMap<String, value::Kind> {
-    input
-        .map
-        .into_iter()
-        .map(|(k, v)| (k, v.kind.expect("`oneof` should not be none")))
-        .collect()
+    input.map.into_iter().map(|(k, v)| (k, v.into())).collect()
 }
 
 pub fn parse_raw_values(values: Vec<Value>) -> Vec<value::Kind> {
-    values
-        .into_iter()
-        .map(|v| v.kind.expect("`oneof` should not be none"))
-        .collect()
+    values.into_iter().map(|v| v.into()).collect()
 }
