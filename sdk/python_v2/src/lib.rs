8986af4823ee3e4071c34bf4e6da15e747111169
Wenxuan Zhang <wenxuangm@gmail.com>
2022-01-19 19:54:34 +0800

    refactor(sdk/py): implement health_check api

--- sdk/python_v2/src/lib.rs
+++ sdk/python_v2/src/lib.rs
@@ -17,13 +17,20 @@ impl Client {
             Python::with_gil(|py| PyCell::new(py, client).map(|py_cell| py_cell.to_object(py)))
         })
     }
+
+    pub fn health_check<'p>(&self, py: Python<'p>) -> PyResult<&'p PyAny> {
+        // Don't panic, it's cheap:
+        // https://github.com/hyperium/tonic/issues/285#issuecomment-595880400
+        let mut inner = OomClient::clone(&self.inner);
+        future_into_py(py, async move { inner.health_check().await.map_err(to_py_execption) })
+    }
 }
 
 pub fn to_py_execption(err: impl std::fmt::Display) -> PyErr {
     PyException::new_err(format!("{}", err))
 }
 
-/// A Python module implemented in Rust.
+/// OomClient python module implemented in Rust.
 #[pymodule]
 fn oomclient(_py: Python, m: &PyModule) -> PyResult<()> {
     unsafe {
