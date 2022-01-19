675d3388aaacf4d65cab6af0eca125d70210ad42
lianxmfor <1034552569@qq.com>
2022-01-19 16:05:02 +0800

    test: more stream push integration test

--- oomcli/test/test_push.sh
+++ oomcli/test/test_push.sh
@@ -14,3 +14,12 @@ oomcli push --entity-key 1 --group user-click --features last_5_click_posts=1,2,
 
 actual=$(oomcli get online --feature user-click.last_5_click_posts,user-click.number_of_user_starred_posts -k 1 -o csv)
 assert_eq "$case" "$expected" "$actual"
+
+case="push stream feature again with difference value"
+expected='
+user,user-click.last_5_click_posts,user-click.number_of_user_starred_posts
+1,"2,3,4,5,6",11
+'
+oomcli push --entity-key 1 --group user-click --features last_5_click_posts=2,3,4,5,6 --features number_of_user_starred_posts=11
+actual=$(oomcli get online --feature user-click.last_5_click_posts,user-click.number_of_user_starred_posts -k 1 -o csv)
+assert_eq "$case" "$expected" "$actual"
